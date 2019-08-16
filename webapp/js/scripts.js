e5f9645a12190ab9eedc3e8159e1f4bfb07f8781
javajigi <javajigi@gmail.com>
2019-08-16 15:03:12 +0900

    fix: add answer to javascript

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -15,11 +15,12 @@ function addAnswer(e) {
   });
 }
 
-function onSuccess(result, status){
+function onSuccess(response, status){
+  const result = response.result;
   if (result.status) {
-	  var answer = json.answer;
-	  var answerTemplate = $("#answerTemplate").html();
-	  var template = answerTemplate.format(answer.writer, new Date(answer.createdDate), answer.contents, answer.answerId, answer.answerId);
+      const answer = response.answer;
+      const answerTemplate = $("#answerTemplate").html();
+      const template = answerTemplate.format(answer.writer, new Date(answer.createdDate), answer.contents, answer.answerId, answer.answerId);
 	  $(".qna-comment-slipp-articles").prepend(template);	  
   } else {
 	  alert(result.message);
