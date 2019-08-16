fe992cef7874839475466ddbb6f71b21739419df
pplenty <newtype26@naver.com>
2019-08-16 23:44:10 +0900

    feat: json view 구현

--- src/main/java/core/mvc/JsonUtils.java
+++ src/main/java/core/mvc/JsonUtils.java
@@ -6,14 +6,28 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 
 public class JsonUtils {
+
+    private static ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
+                .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+                .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
+                .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
+    }
+
     public static <T> T toObject(String json, Class<T> clazz) throws ObjectMapperException {
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.setVisibility(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
-                    .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
             return objectMapper.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new ObjectMapperException(e);
+        }
+    }
+
+    public static String toString(Object value) throws ObjectMapperException {
+        try {
+            return objectMapper.writeValueAsString(value);
         } catch (IOException e) {
             throw new ObjectMapperException(e);
         }
--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,35 @@
 package core.mvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 public class JsonView implements View {
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.setStatus(HttpStatus.OK.value());
+
+        if (model == null || model.isEmpty()) {
+            return;
+        }
+
+        String jsonString = JsonUtils.toString(getFirstIfSingle(model));
+        response.getWriter().write(jsonString);
+    }
+
+    private Object getFirstIfSingle(Map<String, ?> model) {
+
+        if (model.size() > 1) {
+            return model;
+        }
+
+        return model.values().iterator().next();
     }
 }
