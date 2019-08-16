3195db31c40615ac8c63bf2f6cbfe5e441203827
javajigi <javajigi@gmail.com>
2019-08-16 11:30:24 +0900

    refactor: crud library to AcceptanceTest

--- src/test/java/next/controller/UserAcceptanceTest.java
+++ src/test/java/next/controller/UserAcceptanceTest.java
@@ -3,13 +3,12 @@ package next.controller;
 import next.dto.UserCreatedDto;
 import next.dto.UserUpdatedDto;
 import next.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.test.web.reactive.server.EntityExchangeResult;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
+import support.test.NsWebTestClient;
 
 import java.net.URI;
 
@@ -18,61 +17,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UserAcceptanceTest {
     private static final Logger logger = LoggerFactory.getLogger(UserAcceptanceTest.class);
 
+    private NsWebTestClient client;
+
+    @BeforeEach
+    void setUp() {
+        client = NsWebTestClient.of(8080);
+    }
+
     @Test
     @DisplayName("사용자 회원가입/조회/수정/삭제")
     void crud() {
         // 회원가입
         UserCreatedDto expected =
                 new UserCreatedDto("pobi", "password", "포비", "pobi@nextstep.camp");
-        EntityExchangeResult<byte[]> response = client()
-                .post()
-                .uri("/api/users")
-                .body(Mono.just(expected), UserCreatedDto.class)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectBody()
-                .returnResult();
-        URI location = response.getResponseHeaders().getLocation();
+        URI location = client.createResource("/api/users", expected, UserCreatedDto.class);
         logger.debug("location : {}", location); // /api/users?userId=pobi 와 같은 형태로 반환
 
         // 조회
-        User actual = client()
-                .get()
-                .uri(location.toString())
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(User.class)
-                .returnResult().getResponseBody();
+        User actual = client.getResource(location, User.class);
         assertThat(actual.getUserId()).isEqualTo(expected.getUserId());
         assertThat(actual.getName()).isEqualTo(expected.getName());
         assertThat(actual.getEmail()).isEqualTo(expected.getEmail());
 
         // 수정
         UserUpdatedDto updateUser = new UserUpdatedDto("password2", "코난", "conan@nextstep.camp");
-        client()
-                .put()
-                .uri(location.toString())
-                .body(Mono.just(updateUser), UserUpdatedDto.class)
-                .exchange()
-                .expectStatus().isOk();
+        client.updateResource(location, updateUser, UserUpdatedDto.class);
 
-
-        actual = client()
-                .get()
-                .uri(location.toString())
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(User.class)
-                .returnResult().getResponseBody();
+        actual = client.getResource(location, User.class);
         assertThat(actual.getPassword()).isEqualTo(updateUser.getPassword());
         assertThat(actual.getName()).isEqualTo(updateUser.getName());
         assertThat(actual.getEmail()).isEqualTo(updateUser.getEmail());
-    }
-
-    private WebTestClient client() {
-        return WebTestClient
-                .bindToServer()
-                .baseUrl("http://localhost:8080")
-                .build();
     }
 }
