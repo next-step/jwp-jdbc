76f9145c54eb5df2a32b53ec5da0f2498d38ca87
internet200q <internet200@qdillion.com>
2019-08-18 02:15:14 +0900

    jsonView 로직 수정 및 jsonUtil 메소드 추가

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,62 +1,63 @@
 package core.mvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.security.InvalidParameterException;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class JsonView implements View {
-    private static final Logger logger = LoggerFactory.getLogger(JsonView.class);
-
-    public JsonView() {
-    }
 
     @Override
-    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-        String jsonStr = "";
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        if(model.size() != 0){
-            jsonStr = objectMapper.writeValueAsString(getMapToFirstItem(model));
-        }
-
-        if(model.size() > 1){
-            jsonStr = objectMapper.writeValueAsString(model.values().stream()
-                    .map(val -> {
-                        return getObjectToJson(objectMapper, val);
-                    }).collect(Collectors.toList()));
-        }
+    public void render(Map<String, ?> model,
+                       HttpServletRequest request,
+                       HttpServletResponse response) throws Exception {
 
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        PrintWriter printWriter = response.getWriter();
+        sendJson(model, response);
+    }
 
-        printWriter.print(jsonStr);
+    public void sendJson(Map<String, ?> model,
+                                HttpServletResponse response) throws Exception{
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        if(model.size() == 0){
+            sendEmptyValueJson(response);
+            return;
+        }
+
+        if(model.size() == 1){
+            sendFirstValueJson(model, response, objectMapper);
+            return;
+        }
+
+        sendValueJson(model, response, objectMapper);
+    }
+
+    private void sendEmptyValueJson(HttpServletResponse response) throws Exception{
+        PrintWriter printWriter = response.getWriter();
+        printWriter.print("");
         printWriter.flush();
     }
 
-    public Object getMapToFirstItem(Map<String, ?> model){
-        try {
-            return model.values().stream()
-                    .findFirst().orElseThrow(() -> new Exception("값이 비어 있음"));
-        }catch (Exception e){
-            logger.error("getMapToFirstItem Error {}", e.getMessage());
-        }
-        return "";
+    private void sendFirstValueJson(Map<String, ?> model,
+                                             HttpServletResponse response,
+                                             ObjectMapper objectMapper) throws Exception{
+
+        Object value = model.values().stream()
+                .findFirst().orElseThrow(() -> new InvalidParameterException("값이 비어 있음"));
+
+        objectMapper.writeValue(response.getOutputStream(), value);
     }
 
-    public Object getObjectToJson(ObjectMapper objectMapper, Object object){
-        try {
-            return objectMapper.writeValueAsString(object);
-        } catch (Exception e) {
-            logger.error("getObjectToJson Error {}", e.getMessage());
-        }
-        return "";
+    private void sendValueJson(Map<String, ?> model,
+                                        HttpServletResponse response,
+                                        ObjectMapper objectMapper) throws Exception{
+
+        objectMapper.writeValue(response.getOutputStream(), model);
     }
 }
