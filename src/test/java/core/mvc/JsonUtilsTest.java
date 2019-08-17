8d4e772723d25d6ffedd5929ac0c0f55c4488782
Summer <jihan805@coupang.com>
2019-08-17 22:19:06 +0900

    test : add JsonUtilsTest

--- src/test/java/core/mvc/JsonUtilsTest.java
+++ src/test/java/core/mvc/JsonUtilsTest.java
@@ -1,7 +1,13 @@
 package core.mvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
 import study.jackson.Car;
+
+import javax.servlet.ServletOutputStream;
+
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,5 +18,18 @@ public class JsonUtilsTest {
         Car car = JsonUtils.toObject(json, Car.class);
         assertThat(car.getColor()).isEqualTo("Black");
         assertThat(car.getType()).isEqualTo("BMW");
+    }
+
+    @Test
+    void toJson() throws IOException {
+        Car car = new Car("Black", "Sonata");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(car);
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        ServletOutputStream outputStream = response.getOutputStream();
+        JsonUtils.toJson(outputStream, car);
+
+        assertThat(response.getContentAsString()).isEqualTo(json);
     }
 }
