223da9ae838a3d23840a748933ca9111fe31af2f
Summer <jihan805@coupang.com>
2019-08-17 22:10:31 +0900

    refactor : refactor JsonView

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -2,6 +2,7 @@ package core.mvc;
 
 import org.springframework.http.MediaType;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
@@ -12,14 +13,16 @@ public class JsonView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        ServletOutputStream outputStream = response.getOutputStream();
 
         if (model.size() > MINIMUM_MODEL_SIZE) {
-            JsonUtils.toJson(response.getOutputStream(), model);
+            JsonUtils.toJson(outputStream, model);
+            return;
         }
 
         for (String key : model.keySet()) {
             Object obj = model.get(key);
-            JsonUtils.toJson(response.getOutputStream(), obj);
+            JsonUtils.toJson(outputStream, obj);
             return;
         }
     }
