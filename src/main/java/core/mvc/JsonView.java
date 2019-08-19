b5261ee2e174f70d413ef559a316bf624459ebaa
KangHyunGu <hkkang@woowahan.com>
2019-08-19 21:46:12 +0900

    refact : 1단계 -  리팩토링
    - JsonView 메소드 추출하여 코드 정리
    - else if 제거

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -5,33 +5,35 @@ import org.springframework.http.MediaType;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class JsonView implements View {
 
     private static final String UTF_8 = "UTF-8";
 
-    /*
-    Map에 담긴 model 데이터가 1개인 경우 value 값을 반환, 2개 이상인 경우 Map 자체를 JSON으로 변환해 반환한다.
-     */
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
         response.setCharacterEncoding(UTF_8);
 
         final PrintWriter writer = response.getWriter();
+        final String json = parseJson(model);
 
+        writer.print(json);
+        writer.flush();
+    }
+
+    private String parseJson(final Map<String, ?> model) {
         if (model == null || model.size() == 0) {
-            writer.flush();
-            return;
+            return "";
         }
 
         if (model.size() == 1) {
-            model.forEach((key, value) -> writer.print(JsonUtils.toString(value)));
-        } else {
-            writer.print(JsonUtils.toString(model));
+            final ArrayList<?> objects = new ArrayList<>(model.values());
+            return JsonUtils.toString(objects.get(0));
         }
 
-        writer.flush();
+        return JsonUtils.toString(model);
     }
 }
