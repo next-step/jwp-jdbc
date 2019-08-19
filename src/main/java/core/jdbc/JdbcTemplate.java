74ce1c549c225dc1dd84ee774e316e30143cdcc6
internet200q <internet200@qdillion.com>
2019-08-19 23:55:11 +0900

    조회 문 정리

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -1,7 +1,5 @@
 package core.jdbc;
 
-import org.springframework.jdbc.core.PreparedStatementSetter;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -51,47 +49,4 @@ public class JdbcTemplate {
             pstmt.setString(index, String.valueOf(value));
         }
     }
-
-    /*
-
-    private static void initClassValue(PreparedStatement pstmt, Object value, int index) throws SQLException{
-
-    }
-
-    private static boolean isClass(Object object){
-        if(object instanceof Integer){
-            return false;
-        }
-
-        if(object instanceof String){
-            return false;
-        }
-
-        if(object instanceof Long){
-            return false;
-        }
-
-        if (object instanceof Double){
-            return false;
-        }
-
-        if (object instanceof Float){
-            return false;
-        }
-
-        if (object instanceof Boolean){
-            return false;
-        }
-
-        if (object instanceof Byte){
-            return false;
-        }
-
-        if (object instanceof Short){
-            return false;
-        }
-
-        return true;
-    }*/
-
 }
