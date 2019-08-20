310a35687bbead9649a5c07aeffcc5c8150b19fb
Jaeyeonling <jaeyeonling@gmail.com>
2019-08-20 23:57:14 +0900

    [feat] 요구사항 1 구현
    * core.mvc.JsonViewTest의 모든 테스트를 pass하도록 JsonView를 구현한다.

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,42 @@
 package core.mvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 public class JsonView implements View {
-    @Override
-    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final int SIZE_OF_EXTRACT = 1;
+
+    @Override
+    public void render(final Map<String, ?> model,
+                       final HttpServletRequest request,
+                       final HttpServletResponse response) throws Exception {
+        response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE);
+        if (model.isEmpty()) {
+            return;
+        }
+
+        response.getWriter()
+                .write(objectMapper.writeValueAsString(extract(model)));
+    }
+
+    private Object extract(final Map<String, ?> model) {
+        if (model.size() != SIZE_OF_EXTRACT) {
+            return model;
+        }
+
+        return model.entrySet()
+                .stream()
+                .findFirst()
+                .map(Map.Entry::getKey)
+                .map(model::get)
+                .get();
     }
 }
