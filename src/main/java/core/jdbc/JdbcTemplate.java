c155ec15cffa43873e4eb22bbe1d6a508235f09b
Hwatu(박현식) <phs1116@gmail.com>
2019-08-22 03:32:37 +0900

    feat JdbcTemplate 추가, ResultMapper 추가, JdbcSQLException 추가 및 예외변환 DAO에 Template 적용

---
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -0,0 +1,35 @@
+package core.jdbc;
+
+import core.jdbc.exception.JdbcSQLException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by hspark on 2019-08-22.
+ */
+public class JdbcTemplate {
+    public static void command(PreparedStatementCreator preparedStatementCreator) {
+
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = preparedStatementCreator.createPreparedStatement(con)
+        ) {
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new JdbcSQLException(e);
+        }
+    }
+
+    public static <T> T query(PreparedStatementCreator preparedStatementCreator, ResultMapper<T> resultMapper) {
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = preparedStatementCreator.createPreparedStatement(con);
+             ResultSet rs = pstmt.executeQuery()
+        ) {
+            return resultMapper.map(rs);
+        } catch (SQLException e) {
+            throw new JdbcSQLException(e);
+        }
+    }
+}
--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -1,127 +1,72 @@
 package next.dao;
 
-import core.jdbc.ConnectionManager;
+import core.jdbc.JdbcTemplate;
 import next.model.User;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
-    public void insert(User user) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
+    public void insert(User user) {
+        JdbcTemplate.command(con -> {
             String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-            pstmt = con.prepareStatement(sql);
+            PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setString(1, user.getUserId());
             pstmt.setString(2, user.getPassword());
             pstmt.setString(3, user.getName());
             pstmt.setString(4, user.getEmail());
-
-            pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
-        }
+            return pstmt;
+        });
     }
 
-    public void update(User user) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
+    public void update(User user) {
+        JdbcTemplate.command(con -> {
             String sql = "UPDATE USERS SET password = ?, name = ?, email = ? WHERE userId = ?";
-            pstmt = con.prepareStatement(sql);
+            PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setString(1, user.getPassword());
             pstmt.setString(2, user.getName());
             pstmt.setString(3, user.getEmail());
             pstmt.setString(4, user.getUserId());
-
-            pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
-        }
+            return pstmt;
+        });
     }
 
     public List<User> findAll() throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
+
+        return JdbcTemplate.query(con -> {
             String sql = "SELECT userId, password, name, email FROM USERS";
-            pstmt = con.prepareStatement(sql);
-
-            rs = pstmt.executeQuery();
-
-
-            List<User> users = new ArrayList<User>();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            return pstmt;
+        }, rs -> {
+            List<User> users = new ArrayList<>();
             while (rs.next()) {
-                User user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                User user = new User(
+                        rs.getString("userId"),
+                        rs.getString("password"),
+                        rs.getString("name"),
                         rs.getString("email"));
                 users.add(user);
             }
-
             return users;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+        });
 
     }
 
-    public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
+    public User findByUserId(String userId) {
+        return JdbcTemplate.query(con -> {
             String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-            pstmt = con.prepareStatement(sql);
+            PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setString(1, userId);
-
-            rs = pstmt.executeQuery();
-
+            return pstmt;
+        }, rs -> {
             User user = null;
             if (rs.next()) {
                 user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
                         rs.getString("email"));
             }
-
             return user;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+        });
     }
 }
