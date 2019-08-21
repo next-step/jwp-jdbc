601f2e1927e75f8cf17a464904776163af54aa34
dave915 <Htaehyun@gmail.com>
2019-08-22 08:35:50 +0900

    [refector] datasource 싱글톤 방식 수정

--- src/main/java/core/jdbc/ConnectionManager.java
+++ src/main/java/core/jdbc/ConnectionManager.java
@@ -5,27 +5,25 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Objects;
 
 public class ConnectionManager {
     private static final String DB_DRIVER = "org.h2.Driver";
     private static final String DB_URL = "jdbc:h2:mem:jwp-framework";
     private static final String DB_USERNAME = "sa";
     private static final String DB_PW = "";
-    private static DataSource dataSource;
+    private static DataSource INSTANCE = createDataSource();
 
-    public static synchronized DataSource getDataSource() {
-        if (Objects.nonNull(dataSource)) {
-            return dataSource;
-        }
+    public static DataSource getDataSource() {
+        return INSTANCE;
+    }
 
+    private static DataSource createDataSource() {
         BasicDataSource ds = new BasicDataSource();
         ds.setDriverClassName(DB_DRIVER);
         ds.setUrl(DB_URL);
         ds.setUsername(DB_USERNAME);
         ds.setPassword(DB_PW);
-        dataSource = ds;
-        return dataSource;
+        return ds;
     }
 
     public static Connection getConnection() {
