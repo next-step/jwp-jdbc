bbe9bb1cca1f445e5b123981b7542ee25371d0ee
Jinho Yim <jinhoyim@outlook.com>
2019-08-22 09:25:38 +0900

    refactor: 메서드 분리

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,5 +1,6 @@
 package core.mvc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.MediaType;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,18 +9,27 @@ import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        if(model.size() == 0) {
-            return;
-        }
         final PrintWriter writer = response.getWriter();
+        writeJson(model, writer);
+    }
+
+    private void writeJson(Map<String, ?> model, PrintWriter writer) throws JsonProcessingException {
+        if(model.size() == 0)
+            return;
+
+        Object target = pickValueToJson(model);
+        writer.print(JsonUtils.toString(target));
+    }
+
+    private static Object pickValueToJson(Map<String, ?> model) {
         if(model.size() == 1) {
             final Map.Entry<String, ?> next = model.entrySet().iterator().next();
-            final Object value = next.getValue();
-            writer.print(JsonUtils.toString(value));
+            return next.getValue();
         }
-        writer.print(JsonUtils.toString(model));
+        return model;
     }
 }
