a296c5e4ca442747949a4546c1c89996b8f88f7b
nokchax <nokchax@gmail.com>
2020-06-18 20:53:35 +0900

    feat: set content type of response as application/json

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,5 +1,7 @@
 package core.mvc;
 
+import org.springframework.http.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
@@ -7,6 +9,6 @@ import java.util.Map;
 public class JsonView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
     }
 }
