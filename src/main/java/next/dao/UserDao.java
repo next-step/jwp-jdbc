09d53ace1e6a2cdefddbaf62c147a48b7ea02d2a
nokchax <nokchax@gmail.com>
2020-06-21 14:49:09 +0900

    feat: find result set and convert to target dao using reflection

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -1,20 +1,14 @@
 package next.dao;
 
-import core.jdbc.ConnectionManager;
 import core.jdbc.JdbcApi;
 import next.model.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
     private final JdbcApi<User> userDB = new JdbcApi<>(User.class);
 
-    public void insert(User user) throws SQLException {
+    public void insert(User user) {
         userDB.execute(
                 "INSERT INTO USERS VALUES (?, ?, ?, ?)",
                 user.getUserId(),
@@ -34,39 +28,11 @@ public class UserDao {
         );
     }
 
-    public List<User> findAll() throws SQLException {
+    public List<User> findAll() {
         return userDB.findAll("SELECT userId, password, name, email FROM USERS");
     }
 
-    public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
-
-            rs = pstmt.executeQuery();
-
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-            }
-
-            return user;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+    public User findByUserId(String userId) {
+        return userDB.findOne("SELECT userId, password, name, email FROM USERS WHERE userid=?", userId);
     }
 }
