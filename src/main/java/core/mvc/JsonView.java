ed79c62335b35bad5bbbbcbfa63f1ada68e2bc07
nokchax <nokchax@gmail.com>
2020-06-21 15:39:45 +0900

    refactor: apply review, flush writer

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
@@ -23,7 +24,10 @@ public class JsonView implements View {
 
         String body = writeValueAsString(model);
         response.setContentLength(body.getBytes(StandardCharsets.UTF_8).length);
-        response.getWriter().write(body);
+
+        PrintWriter writer = response.getWriter();
+        writer.write(body);
+        writer.flush();
     }
 
     private String writeValueAsString(Map<String, ?> model) throws JsonProcessingException {
