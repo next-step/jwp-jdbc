1614a457b72e23f6cb4a00565cabbf5d76027a2e
nokchax <nokchax@gmail.com>
2020-06-22 20:07:03 +0900

    refactor: apply constant

--- src/test/java/core/mvc/intercepter/InterceptorTest.java
+++ src/test/java/core/mvc/intercepter/InterceptorTest.java
@@ -13,6 +13,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("인터셉터 테스트")
 class InterceptorTest {
+    private static final String TEST = "test";
+
     private TestInterceptor testInterceptor;
     private MockHttpServletRequest request;
     private MockHttpServletResponse response;
@@ -27,30 +29,30 @@ class InterceptorTest {
     @Test
     @DisplayName("인터셉터는 controller 단 기능 수행 이전에 역할을 수행할 수 있어야 한다")
     void preProcess() {
-        assertThat(request.getAttribute("test")).isNull();
+        assertThat(request.getAttribute(TEST)).isNull();
         testInterceptor.preProcess(request, response);
-        assertThat(request.getAttribute("test")).isNotNull();
-        assertThat(request.getAttribute("test")).isEqualTo(true);
+        assertThat(request.getAttribute(TEST)).isNotNull();
+        assertThat(request.getAttribute(TEST)).isEqualTo(true);
     }
 
     @Test
     @DisplayName("인터셉터는 controller 단 기능 수행 이후에 역할을 수행할 수 있어야 한다.")
     void postProcess() {
-        assertThat(request.getAttribute("test")).isNull();
+        assertThat(request.getAttribute(TEST)).isNull();
         testInterceptor.postProcess(request, response);
-        assertThat(request.getAttribute("test")).isNotNull();
-        assertThat(request.getAttribute("test")).isEqualTo(false);
+        assertThat(request.getAttribute(TEST)).isNotNull();
+        assertThat(request.getAttribute(TEST)).isEqualTo(false);
     }
 
     private static class TestInterceptor implements Interceptor {
         @Override
         public void preProcess(HttpServletRequest request, HttpServletResponse response) {
-            request.setAttribute("test", true);
+            request.setAttribute(TEST, true);
         }
 
         @Override
         public void postProcess(HttpServletRequest request, HttpServletResponse response) {
-            request.setAttribute("test", false);
+            request.setAttribute(TEST, false);
         }
     }
 }
