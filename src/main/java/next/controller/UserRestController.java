59c7f57851f158939faa15e6e90aff6676723920
bactoria <bactoria@gmail.com>
2020-06-23 11:41:12 +0900

    refactor : UserRestController 가독성 개선

--- src/main/java/next/controller/UserRestController.java
+++ src/main/java/next/controller/UserRestController.java
@@ -8,7 +8,6 @@ import core.db.DataBase;
 import core.mvc.JsonUtils;
 import core.mvc.JsonView;
 import core.mvc.ModelAndView;
-import next.dto.UserCreatedDto;
 import next.dto.UserDetailResponseDto;
 import next.dto.UserUpdatedDto;
 import next.model.User;
@@ -17,6 +16,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.stream.Collectors;
 
 @Controller
@@ -24,42 +24,43 @@ public class UserRestController {
     private static final Logger logger = LoggerFactory.getLogger(UserRestController.class);
 
     @RequestMapping(value = "/api/users", method = RequestMethod.POST)
-    public ModelAndView create(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-
-        User user = JsonUtils.toObject(req.getReader().lines().collect(Collectors.joining(System.lineSeparator())), User.class);
+    public ModelAndView create(HttpServletRequest req) throws IOException {
+        final User user = JsonUtils.toObject(req.getReader().lines().collect(Collectors.joining(System.lineSeparator())), User.class);
 
         DataBase.addUser(user);
 
-        ModelAndView mav = new ModelAndView(new JsonView(201, "/api/users?userId=" + user.getUserId()));
-        return mav;
+        return new ModelAndView(new JsonView(HttpServletResponse.SC_CREATED, "/api/users?userId=" + user.getUserId()));
     }
 
     @RequestMapping(value = "/api/users", method = RequestMethod.GET)
-    public ModelAndView read(@RequestParam String userId) throws Exception {
+    public ModelAndView read(@RequestParam String userId) {
+        logger.debug("userId : {}", userId);
 
         final User user = DataBase.findUserById(userId);
         final UserDetailResponseDto body = new UserDetailResponseDto(user);
 
-        ModelAndView mav = new ModelAndView(new JsonView(200));
+        final ModelAndView mav = new ModelAndView(new JsonView(HttpServletResponse.SC_OK));
         mav.addObject("body", body);
         return mav;
     }
 
     @RequestMapping(value = "/api/users", method = RequestMethod.PUT)
-    public ModelAndView update(@RequestParam String userId, HttpServletRequest req, HttpServletResponse resp) throws Exception {
+    public ModelAndView update(@RequestParam String userId, HttpServletRequest req) throws IOException {
+        logger.debug("userId : {}", userId);
 
+        final UserUpdatedDto userUpdatedDto = JsonUtils.toObject(req.getReader().lines().collect(Collectors.joining(System.lineSeparator())), UserUpdatedDto.class);
         final User user = DataBase.findUserById(userId);
 
         if (user == null) {
             throw new RuntimeException("해당 유저가 없습니다. userId: " + userId);
         }
 
-        UserUpdatedDto userUpdatedDto = JsonUtils.toObject(req.getReader().lines().collect(Collectors.joining(System.lineSeparator())), UserUpdatedDto.class);
+        DataBase.addUser(new User(userId, user.getPassword(), userUpdatedDto.getName(), userUpdatedDto.getEmail()));
 
-        DataBase.addUser(new User(user.getUserId(), user.getPassword(), userUpdatedDto.getName(), userUpdatedDto.getEmail()));
+        final User updatedUser = DataBase.findUserById(userId);
 
-        ModelAndView mav = new ModelAndView(new JsonView(200));
-        mav.addObject("user", DataBase.findUserById(userId));
+        final ModelAndView mav = new ModelAndView(new JsonView(HttpServletResponse.SC_OK));
+        mav.addObject("user", updatedUser);
         return mav;
     }
 
