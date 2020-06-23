3c678c88ed04df63b40733e208d42f74b824bb3a
nokchax <nokchax@gmail.com>
2020-06-23 13:45:42 +0900

    refactor: make getInstance static to get globally

--- src/test/java/core/containter/SingletonContainerTest.java
+++ src/test/java/core/containter/SingletonContainerTest.java
@@ -1,26 +1,19 @@
 package core.containter;
 
-import core.jdbc.ConnectionManager;
 import core.jdbc.JdbcTemplate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("싱글턴 컨테이너")
 class SingletonContainerTest {
-    private SingletonContainer singletonContainer = new SingletonContainer(
-            Collections.singletonList(new JdbcTemplate(ConnectionManager.getDataSource()))
-    );
-
 
     @Test
     @DisplayName("같은 객체를 리턴하는지")
     void getInstance() {
-        JdbcTemplate one = singletonContainer.getInstance(JdbcTemplate.class);
-        JdbcTemplate another = singletonContainer.getInstance(JdbcTemplate.class);
+        JdbcTemplate one = SingletonContainer.getInstance(JdbcTemplate.class);
+        JdbcTemplate another = SingletonContainer.getInstance(JdbcTemplate.class);
 
         assertThat(one).isEqualTo(another);
     }
