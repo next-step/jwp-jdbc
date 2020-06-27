3cb98c5b0e6166772bebaf4706d312fdb85523d3
liquidjoo <liquidjoo@gmail.com>
2020-06-27 14:08:46 +0900

    feat : Map에 담긴 model 데이터가 1개인 경우 value 값을 반환, 2개 이상인 경우 Map 자체를 JSON으로 변환해 반환

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -11,8 +11,12 @@ public class JsonView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setHeader("content-type", MediaType.APPLICATION_JSON_VALUE);
-
         Set<String> keys = model.keySet();
+
+        if (keys.size() > 1) {
+            response.getWriter().println(JsonUtils.toJsonAsString(model));
+            return;
+        }
         for (String key : keys) {
             response.getWriter().println(JsonUtils.toJsonAsString(model.get(key)));
         }
