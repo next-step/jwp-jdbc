46106c7f16962937fe9ec371d93039a6f6fdd7db
bactoria <bactoria@gmail.com>
2020-06-28 14:47:07 +0900

    feat : userDao 구현

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -3,10 +3,7 @@ package next.dao;
 import core.jdbc.ConnectionManager;
 import next.model.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +13,7 @@ public class UserDao {
         PreparedStatement pstmt = null;
         try {
             con = ConnectionManager.getConnection();
-            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+            final String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, user.getUserId());
             pstmt.setString(2, user.getPassword());
@@ -36,12 +33,60 @@ public class UserDao {
     }
 
     public void update(User user) throws SQLException {
-        // TODO 구현 필요함.
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = ConnectionManager.getConnection();
+            final String sql = "UPDATE USERS SET password=?, name=?, email=? WHERE userId=?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
+
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
     }
 
     public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        return new ArrayList<User>();
+        List<User> result = new ArrayList<>();
+
+        Connection con = null;
+        Statement stmt = null;
+        try {
+            con = ConnectionManager.getConnection();
+            final String sql = "SELECT * FROM USERS";
+            stmt = con.createStatement();
+
+            final ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                final String userId = rs.getString("userId");
+                final String password = rs.getString("password");
+                final String name = rs.getString("name");
+                final String email = rs.getString("email");
+
+                final User user = new User(userId, password, name, email);
+                result.add(user);
+            }
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
+        return result;
     }
 
     public User findByUserId(String userId) throws SQLException {
@@ -50,7 +95,7 @@ public class UserDao {
         ResultSet rs = null;
         try {
             con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+            final String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, userId);
 
