090fed405141da8b85e4ceb976d6a74a028f724f
kjs4395 <kjs4395@gmail.com>
2020-07-02 10:27:22 +0900

    refactor (apply review) : content type 메서드 내 지역변수참조 변경

--- src/main/java/next/controller/UserApiController.java
+++ src/main/java/next/controller/UserApiController.java
@@ -7,13 +7,10 @@ import core.annotation.web.RequestParam;
 import core.db.DataBase;
 import core.mvc.JsonView;
 import core.mvc.ModelAndView;
-
 import next.dto.UserCreatedDto;
 import next.dto.UserUpdatedDto;
 import next.model.User;
 import org.springframework.http.HttpStatus;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created By kjs4395 on 6/30/20
@@ -25,7 +22,7 @@ public class UserApiController {
     public ModelAndView createUser(UserCreatedDto userCreatedDto) {
         DataBase.addUser(User.userCreateDtoToUser(userCreatedDto));
 
-        return new ModelAndView(new JsonView(HttpStatus.CREATED, "/api/users?userId="+userCreatedDto.getUserId()));
+        return new ModelAndView(new JsonView(HttpStatus.CREATED, "/api/users?userId=" + userCreatedDto.getUserId()));
     }
 
     @RequestMapping(value = "/api/users", method = RequestMethod.GET)
