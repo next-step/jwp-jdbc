106ab231c7816c946bec7d2f30fca646fa0e46f5
ka0oll <jjyoung0216@naver.com>
2020-07-03 21:53:15 +0900

    feat @RequestBody resolver구현

---
+++ src/main/java/core/mvc/tobe/support/RequestBodyArgumentResolver.java
@@ -0,0 +1,31 @@
+package core.mvc.tobe.support;
+
+import core.annotation.web.RequestBody;
+import core.mvc.JsonUtils;
+import core.mvc.tobe.MethodParameter;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
+
+public class RequestBodyArgumentResolver extends AbstractAnnotationArgumentResolver {
+    @Override
+    public boolean supports(MethodParameter methodParameter) {
+        return supportAnnotation(methodParameter, RequestBody.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, HttpServletRequest request, HttpServletResponse response) {
+        return JsonUtils.toObject(parseBody(request), methodParameter.getType());
+    }
+
+    private String parseBody(HttpServletRequest request) {
+        try {
+            byte[] body = IOUtils.toByteArray(request.getInputStream());
+            return new String(body, request.getCharacterEncoding());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+}
