6b040689b6828bab853eb6e95e32fe31f0d5451f
ka0oll <jjyoung0216@naver.com>
2020-07-04 13:34:07 +0900

    refactor step 1 리뷰 사항 반영

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,13 @@
 package core.mvc;
 
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.MediaType;
 
 public class JsonView implements View {
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setCharacterEncoding("utf-8");
@@ -14,15 +15,14 @@ public class JsonView implements View {
         response.getWriter().write(createBody(model));
     }
 
-    private String createBody(Map<String, ?> model){
-        if(model.isEmpty()){
+    private String createBody(Map<String, ?> model) {
+        if (model.isEmpty()) {
             return StringUtils.EMPTY;
         }
 
         Object target = model;
-        if(model.size() == 1){
-            String key = model.keySet().iterator().next();
-            target = model.get(key);
+        if (model.size() == 1) {
+            target = model.values().iterator().next();
         }
         return JsonUtils.toJson(target);
     }
