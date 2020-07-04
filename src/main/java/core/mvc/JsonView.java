09efbf5f89b73a375d03792c67060836e2f31ce0
LeeSeungHee <palmseung@gmail.com>
2020-07-05 01:51:29 +0900

    Feat (JsonView) : add feat to set ContentType to application/json

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,17 @@
 package core.mvc;
 
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Map;
 
 public class JsonView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
     }
 }
