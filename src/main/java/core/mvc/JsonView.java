5d0413a34a8dd191ecd97488201af26d5621ad4c
kjm <jm.kang@Qualson.com>
2020-07-05 21:35:34 +0900

    add contentType

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,22 @@
 package core.mvc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 public class JsonView implements View {
+    private static final Logger logger = LoggerFactory.getLogger(JsonView.class);
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String contentType = response.getContentType();
+        if (contentType == null) {
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        }
 
     }
 }
