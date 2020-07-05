7742a000ba2d7e91e566a50370d3644f25e89b96
LeeSeungHee <palmseung@gmail.com>
2020-07-06 00:41:04 +0900

    Feat (ApiUserController) : add feat to retrieve user

--- src/main/java/next/controller/ApiUserController.java
+++ src/main/java/next/controller/ApiUserController.java
@@ -3,11 +3,13 @@ package next.controller;
 import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
+import core.annotation.web.RequestParam;
 import core.db.DataBase;
 import core.mvc.JsonView;
 import core.mvc.ModelAndView;
 import next.model.User;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.http.HttpServletResponse;
@@ -22,6 +24,18 @@ public class ApiUserController {
 
         response.setStatus(201);
         response.setHeader(HttpHeaders.LOCATION, BASE_URL_USER_API + "?userId=" + user.getUserId());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
         return new ModelAndView(new JsonView());
+    }
+
+    @RequestMapping(value = BASE_URL_USER_API, method = RequestMethod.GET)
+    public ModelAndView retrieve(@RequestParam String userId) {
+        User userById = DataBase.findUserById(userId);
+
+        ModelAndView modelAndView = new ModelAndView(new JsonView());
+        modelAndView.addObject("user", userById);
+
+        return modelAndView;
     }
 }
