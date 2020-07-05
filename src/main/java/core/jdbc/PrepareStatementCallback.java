223bd075579ec9f4b64eff12049b4a1093cc0ca2
ka0oll <60064643+ka0oll@users.noreply.github.com>
2020-07-05 21:17:01 +0900

    refactor rs.getFetchSize 비교 제거 (#86)

--- src/main/java/core/jdbc/PrepareStatementCallback.java
+++ src/main/java/core/jdbc/PrepareStatementCallback.java
@@ -3,6 +3,7 @@ package core.jdbc;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 interface PrepareStatementCallback<T> {
     T doInPrepareStatement(PreparedStatement pstmt) throws SQLException;
 }
