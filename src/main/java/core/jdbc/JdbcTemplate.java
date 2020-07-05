0ebdbb8372a1f3ec6abb1d617f829d013fa41ec0
dellife <seheekwon21@gmail.com>
2020-07-05 13:58:57 +0900

    [refactor] try-with-resources 적용

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -12,94 +12,52 @@ import java.util.List;
 public class JdbcTemplate {
 
     public void execute(String sql, Object... args) throws JdbcRuntimeException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
-            for (int i = 0; i < args.length; i++) {
-                if (args[i] instanceof Integer) {
-                    pstmt.setInt(i + 1, (int) args[i]);
-                } else if (args[i] instanceof String) {
-                    pstmt.setString(i + 1, (String) args[i]);
-                }
-            }
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql);) {
+            setArguments(pstmt, args);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new JdbcRuntimeException(e);
-        } finally {
-            close(con, pstmt, null);
         }
     }
 
     public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) throws JdbcRuntimeException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
-            for (int i = 0; i < args.length; i++) {
-                if (args[i] instanceof Integer) {
-                    pstmt.setInt(i + 1, (int) args[i]);
-                } else if (args[i] instanceof String) {
-                    pstmt.setString(i + 1, (String) args[i]);
-                }
-            }
-            rs = pstmt.executeQuery();
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql);) {
+            setArguments(pstmt, args);
+            ResultSet rs = pstmt.executeQuery();
             if (rs.next()) {
                 return rowMapper.mapRow(rs);
             }
             return null;
         } catch (SQLException e) {
             throw new JdbcRuntimeException(e);
-        } finally {
-            close(con, pstmt, rs);
         }
     }
 
     public <T> List<T> queryForList(String sql, RowMapper<T> rowMapper, Object... args) throws JdbcRuntimeException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
-            for (int i = 0; i < args.length; i++) {
-                if (args[i] instanceof Integer) {
-                    pstmt.setInt(i + 1, (int) args[i]);
-                } else if (args[i] instanceof String) {
-                    pstmt.setString(i + 1, (String) args[i]);
-                }
-            }
-            List<T> rows = new ArrayList<>();
-            rs = pstmt.executeQuery();
+        List<T> rows = new ArrayList<>();
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql);) {
+            setArguments(pstmt, args);
+            ResultSet rs = pstmt.executeQuery();
             if (rs.next()) {
                 rows.add(rowMapper.mapRow(rs));
             }
             return rows;
         } catch (SQLException e) {
             throw new JdbcRuntimeException(e);
-        } finally {
-            close(con, pstmt, rs);
         }
     }
 
-
-    private void close(Connection con, PreparedStatement pstmt, ResultSet rs) throws JdbcRuntimeException {
-        try {
-            if (rs != null) {
-                rs.close();
+    private void setArguments(PreparedStatement pstmt, Object[] args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof Integer) {
+                pstmt.setInt(i + 1, (int) args[i]);
+            } else if (args[i] instanceof String) {
+                pstmt.setString(i + 1, (String) args[i]);
             }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-
-        } catch (SQLException e) {
-            throw new JdbcRuntimeException(e);
         }
     }
+
 }
