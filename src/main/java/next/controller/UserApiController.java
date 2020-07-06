7c02fde4a79ce06d9861f45c120ca88c71dff807
kjm <jm.kang@Qualson.com>
2020-07-06 12:35:53 +0900

    feature: 회원가입 controller 추가

---
+++ src/main/java/next/controller/UserApiController.java
@@ -0,0 +1,76 @@
+package next.controller;
+
+import core.annotation.web.Controller;
+import core.annotation.web.RequestMapping;
+import core.annotation.web.RequestMethod;
+import core.db.DataBase;
+import core.mvc.JsonUtils;
+import core.mvc.JsonView;
+import core.mvc.ModelAndView;
+import next.dto.UserCreatedDto;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+@Controller
+public class UserApiController {
+    private static final Logger logger = LoggerFactory.getLogger(UserApiController.class);
+
+    @RequestMapping(value = "/api/users", method = RequestMethod.POST)
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String json = getBody(request);
+
+        UserCreatedDto userCreatedDto = JsonUtils.toObject(json, UserCreatedDto.class);
+        logger.debug("{}, {}", json, userCreatedDto);
+
+        User user = new User(userCreatedDto.getUserId(), userCreatedDto.getPassword(), userCreatedDto.getName(), userCreatedDto.getEmail());
+
+        DataBase.addUser(user);
+
+        response.setHeader("Location", "/api/users?userId=" + user.getUserId());
+        response.setStatus(HttpStatus.CREATED.value());
+        return new ModelAndView(new JsonView());
+    }
+
+    private String getBody(HttpServletRequest request) throws IOException {
+
+        String body = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } catch (IOException ex) {
+            throw ex;
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException ex) {
+                    throw ex;
+                }
+            }
+        }
+
+        body = stringBuilder.toString();
+        return body;
+    }
+}
