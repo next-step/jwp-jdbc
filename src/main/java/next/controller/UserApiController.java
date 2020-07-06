51b634a9f54ef0bb7fa5156fc91a35d7f8677b59
kjm <jm.kang@Qualson.com>
2020-07-06 14:40:01 +0900

    refactoring: request body read 변경

--- src/main/java/next/controller/UserApiController.java
+++ src/main/java/next/controller/UserApiController.java
@@ -1,11 +1,11 @@
 package next.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
 import core.annotation.web.RequestParam;
 import core.db.DataBase;
-import core.mvc.JsonUtils;
 import core.mvc.JsonView;
 import core.mvc.ModelAndView;
 import next.dto.UserCreatedDto;
@@ -16,10 +16,6 @@ import org.springframework.http.HttpStatus;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 @Controller
 public class UserApiController {
@@ -27,10 +23,8 @@ public class UserApiController {
 
     @RequestMapping(value = "/api/users", method = RequestMethod.POST)
     public ModelAndView save(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        String json = getBody(request);
-
-        UserCreatedDto userCreatedDto = JsonUtils.toObject(json, UserCreatedDto.class);
-        logger.debug("{}, {}", json, userCreatedDto);
+        ObjectMapper objectMapper = new ObjectMapper();
+        UserCreatedDto userCreatedDto = objectMapper.readValue(request.getInputStream(), UserCreatedDto.class);
 
         User user = new User(userCreatedDto.getUserId(), userCreatedDto.getPassword(), userCreatedDto.getName(), userCreatedDto.getEmail());
 
@@ -53,10 +47,12 @@ public class UserApiController {
     @RequestMapping(value = "/api/users", method = RequestMethod.PUT)
     public ModelAndView update(@RequestParam String userId, HttpServletRequest request, HttpServletResponse response) throws Exception {
         User user = DataBase.findUserById(userId);
-        String json = getBody(request);
-        User userParameter = JsonUtils.toObject(json, User.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        UserCreatedDto userCreatedDto = objectMapper.readValue(request.getInputStream(), UserCreatedDto.class);
 
+        User userParameter = new User(userCreatedDto.getUserId(), userCreatedDto.getPassword(), userCreatedDto.getName(), userCreatedDto.getEmail());
         user.update(userParameter);
+
         DataBase.addUser(user);
 
         ModelAndView modelAndView = new ModelAndView(new JsonView());
@@ -64,37 +60,4 @@ public class UserApiController {
         return modelAndView;
     }
 
-    private String getBody(HttpServletRequest request) throws IOException {
-
-        String body = null;
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = null;
-
-        try {
-            InputStream inputStream = request.getInputStream();
-            if (inputStream != null) {
-                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                char[] charBuffer = new char[128];
-                int bytesRead = -1;
-                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
-                    stringBuilder.append(charBuffer, 0, bytesRead);
-                }
-            } else {
-                stringBuilder.append("");
-            }
-        } catch (IOException ex) {
-            throw ex;
-        } finally {
-            if (bufferedReader != null) {
-                try {
-                    bufferedReader.close();
-                } catch (IOException ex) {
-                    throw ex;
-                }
-            }
-        }
-
-        body = stringBuilder.toString();
-        return body;
-    }
 }
