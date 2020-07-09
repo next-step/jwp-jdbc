9aa33395a1d417832c8eaf542b0779d001b7cba4
kjm <jm.kang@Qualson.com>
2020-07-09 14:50:59 +0900

    refactoring: 파라미터 리펙토링

--- src/main/java/core/jdbc/QuerySetter.java
+++
@@ -1,8 +0,0 @@
-package core.jdbc;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface QuerySetter {
-    void queryValues(PreparedStatement pstmt) throws SQLException;
-}
