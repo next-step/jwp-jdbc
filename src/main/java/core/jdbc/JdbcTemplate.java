0d804626d6d06a6f65f3d1f990086f3cb3c6265f
dellife <44809525+dellife@users.noreply.github.com>
2020-07-12 04:57:10 +0900

    Step3 JDBC 라이브러리 구현(힌트) (#97)
    * [refactor] 코드리뷰 반영
    
    * [feature] PreparedStatementSetter 추가
    
    * [refactor] 순서 변경, 정렬

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -1,6 +1,7 @@
 package core.jdbc;
 
 import core.jdbc.exception.JdbcRuntimeException;
+import org.springframework.util.CollectionUtils;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -8,38 +9,48 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class JdbcTemplate {
 
     public void execute(String sql, Object... args) throws JdbcRuntimeException {
+        execute(sql, createPreparedStatementSetter(args));
+    }
+
+    public void execute(String sql, PreparedStatementSetter pstmtSetter) throws JdbcRuntimeException {
         try (Connection con = ConnectionManager.getConnection();
              PreparedStatement pstmt = con.prepareStatement(sql);) {
-            setArguments(pstmt, args);
+            pstmtSetter.values(pstmt);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new JdbcRuntimeException(e);
         }
     }
 
-    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) throws JdbcRuntimeException {
-        try (Connection con = ConnectionManager.getConnection();
-             PreparedStatement pstmt = con.prepareStatement(sql);) {
-            setArguments(pstmt, args);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                return rowMapper.mapRow(rs);
-            }
-            return null;
-        } catch (SQLException e) {
-            throw new JdbcRuntimeException(e);
+    public <T> Optional<T> queryForObject(String sql, RowMapper<T> rowMapper, Object... args) throws JdbcRuntimeException {
+        return queryForObject(sql, rowMapper, createPreparedStatementSetter(args));
+    }
+
+    public <T> Optional<T> queryForObject(String sql, RowMapper<T> rowMapper, PreparedStatementSetter pstmtSetter) throws JdbcRuntimeException {
+        List<T> result = queryForList(sql, rowMapper, pstmtSetter);
+        if (CollectionUtils.isEmpty(result)) {
+            return Optional.empty();
         }
+        if (result.size() > 1) {
+            throw new IllegalArgumentException("데이터가 2건 이상 조회될 수 없습니다.");
+        }
+        return Optional.of(result.get(0));
     }
 
     public <T> List<T> queryForList(String sql, RowMapper<T> rowMapper, Object... args) throws JdbcRuntimeException {
+        return queryForList(sql, rowMapper, createPreparedStatementSetter(args));
+    }
+
+    public <T> List<T> queryForList(String sql, RowMapper<T> rowMapper, PreparedStatementSetter pstmtSetter) throws JdbcRuntimeException {
         List<T> rows = new ArrayList<>();
         try (Connection con = ConnectionManager.getConnection();
              PreparedStatement pstmt = con.prepareStatement(sql);) {
-            setArguments(pstmt, args);
+            pstmtSetter.values(pstmt);
             ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
                 rows.add(rowMapper.mapRow(rs));
@@ -50,14 +61,12 @@ public class JdbcTemplate {
         }
     }
 
-    private void setArguments(PreparedStatement pstmt, Object[] args) throws SQLException {
-        for (int i = 0; i < args.length; i++) {
-            if (args[i] instanceof Integer) {
-                pstmt.setInt(i + 1, (int) args[i]);
-            } else if (args[i] instanceof String) {
-                pstmt.setString(i + 1, (String) args[i]);
+    private PreparedStatementSetter createPreparedStatementSetter(Object[] args) {
+        return pstmt -> {
+            for (int i = 0; i < args.length; i++) {
+                pstmt.setObject(i + 1, args[i]);
             }
-        }
+        };
     }
 
 }
