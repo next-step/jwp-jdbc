9206d12bb6a7dd320fe0a63be08bbef876456ae2
kjm <jm.kang@Qualson.com>
2020-07-15 17:27:02 +0900

    feature: 컨트롤러 실행속도 측정

--- src/main/java/core/interceptor/TimeCheckInterceptor.java
+++ src/main/java/core/interceptor/TimeCheckInterceptor.java
@@ -4,6 +4,7 @@ import core.mvc.HandlerInterceptor;
 import core.mvc.ModelAndView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,14 +13,19 @@ public class TimeCheckInterceptor implements HandlerInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(TimeCheckInterceptor.class);
 
+    StopWatch stopWatch = new StopWatch();
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response) {
         logger.debug("interceptor preHandle");
+        stopWatch.start();
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, ModelAndView modelAndView) {
-        logger.debug("interceptor postHandle");
+        stopWatch.stop();
+
+        logger.debug("interceptor postHandle, time: {}", stopWatch.getLastTaskInfo().getTimeSeconds());
     }
 }
