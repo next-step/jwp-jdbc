4323ebf8633bb9a332f6e01170ffa3b1a2325a67
kjm <jm.kang@Qualson.com>
2020-07-15 16:45:32 +0900

    feature: 인터셉터 동작 확인

--- src/main/java/core/interceptor/TimeCheckInterceptor.java
+++ src/main/java/core/interceptor/TimeCheckInterceptor.java
@@ -2,18 +2,24 @@ package core.interceptor;
 
 import core.mvc.HandlerInterceptor;
 import core.mvc.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class TimeCheckInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(TimeCheckInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response) {
-        return false;
+        logger.debug("interceptor preHandle");
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, ModelAndView modelAndView) {
-
+        logger.debug("interceptor postHandle");
     }
 }
