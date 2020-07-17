b2281cb40277e70837e422a85df24948b216a485
crystal_rim <remember2889@gmail.com>
2020-07-17 17:26:46 +0900

    refactor(JsonUtils): 매번 생성되는 objectMapper 정적변수화

--- src/main/java/core/mvc/JsonUtils.java
+++ src/main/java/core/mvc/JsonUtils.java
@@ -6,13 +6,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 
 public class JsonUtils {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        objectMapper.setVisibility(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
+                .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+                .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
+                .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
+    }
+
     public static <T> T toObject(String json, Class<T> clazz) throws ObjectMapperException {
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.setVisibility(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
-                    .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
             return objectMapper.readValue(json, clazz);
         } catch (IOException e) {
             throw new ObjectMapperException(e);
@@ -21,11 +25,6 @@ public class JsonUtils {
 
     public static String toJsonString(Object object) {
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.setVisibility(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
-                    .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
             return objectMapper.writeValueAsString(object);
         } catch (IOException e) {
             throw new ObjectMapperException(e);
