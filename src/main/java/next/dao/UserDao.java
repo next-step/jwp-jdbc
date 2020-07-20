cb07dd380556fffe3ce419c19c6f15eadd3e601f
SujungRim <remember2889@gmail.com>
2020-07-20 21:27:41 +0900

    refactor(UserDao): sql문 상수로

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -8,6 +8,10 @@ import next.model.User;
 import java.util.List;
 
 public class UserDao {
+    public static final String USER_INSERT_SQL = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+    public static final String USER_UPDATE_SQL = "UPDATE USERS SET password = ?, name = ? , email = ? WHERE userId = ?";
+    public static final String USER_FIND_ALL_SQL = "SELECT userId, password, name, email FROM USERS";
+    public static final String USER_FIND_BY_ID_SQL = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -16,22 +20,18 @@ public class UserDao {
     }
 
     public void insert(User user) {
-        String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-        this.jdbcTemplate.update(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+        this.jdbcTemplate.update(USER_INSERT_SQL, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
     }
 
     public void update(User user) {
-        String sql = "UPDATE USERS SET password = ?, name = ? , email = ? WHERE userId = ?";
-        this.jdbcTemplate.update(sql, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
+        this.jdbcTemplate.update(USER_UPDATE_SQL, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
     }
 
     public List<User> findAll() {
-        String sql = "SELECT userId, password, name, email FROM USERS";
-        return this.jdbcTemplate.queryForList(sql, new UserMapper());
+        return this.jdbcTemplate.queryForList(USER_FIND_ALL_SQL, new UserMapper());
     }
 
     public User findByUserId(String userId) {
-        String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-        return this.jdbcTemplate.queryForObject(sql, new UserMapper(), userId);
+        return this.jdbcTemplate.queryForObject(USER_FIND_BY_ID_SQL, new UserMapper(), userId);
     }
 }
