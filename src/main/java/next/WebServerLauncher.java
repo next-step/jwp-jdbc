f2c35786a5a71b8c8249420efea4d5ce5f4511ff
javajigi <javajigi@gmail.com>
2022-05-30 13:55:23 +0900

    chore: update the latest library, framework version

--- src/main/java/next/WebServerLauncher.java
+++ src/main/java/next/WebServerLauncher.java
@@ -1,6 +1,10 @@
 package next;
 
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,9 +16,20 @@ public class WebServerLauncher {
     public static void main(String[] args) throws Exception {
         String webappDirLocation = "webapp/";
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
 
-        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        File additionWebInfClasses = new File("build/classes/java/main");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
         logger.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
 
         tomcat.start();
--- src/main/resources/startdb.sh
+++ src/main/resources/startdb.sh
@@ -1,1 +1,1 @@
-java -cp h2-1.4.199.jar org.h2.tools.Server
+java -cp h2-2.1.212.jar org.h2.tools.Server
