e6c7434ffa5c21b2ee5732b51197bfc072ac8303
dongchul <sorkehdcjf@gmail.com>
2022-07-14 01:29:21 +0900

    Step1 - REST API 및 테스트 리팩토링 리뷰요청 드립니다.  (#114)
    * feat: JsonView 구현
    
    * feat: User Acceptance Controller 구현

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,39 @@
 package core.mvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class JsonView implements View {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
+        if (model.isEmpty()) {
+            return;
+        }
+
+        if (model.size() == 1) {
+            parseSingleResponse(model, response);
+            return;
+        }
+
+        objectMapper.writeValue(response.getOutputStream(), model);
+    }
+
+    private void parseSingleResponse(Map<String, ?> model, HttpServletResponse response) throws IOException {
+        String key = model.keySet()
+                        .stream()
+                        .findAny()
+                        .get();
+
+        objectMapper.writeValue(response.getOutputStream(), model.get(key));
     }
 }
