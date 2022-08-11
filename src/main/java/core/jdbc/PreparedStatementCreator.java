0131eafa58121c0ad13ceeba40837b95b68ece19
seondongpyo <seondongpyo@gmail.com>
2022-08-11 23:14:53 +0900

    feat: 함수형 인터페이스 애노테이션 추가

--- src/main/java/core/jdbc/PreparedStatementCreator.java
+++ src/main/java/core/jdbc/PreparedStatementCreator.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementCreator {
+
     PreparedStatement createPreparedStatement(Connection con) throws SQLException;
 }
