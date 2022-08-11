42cc6787e8b4f9bc804b7ce18e0a95f3562d84d0
devson <sonyc5720@gmail.com>
2022-08-11 20:49:14 +0900

    refactor: 중복 코드 제거

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -1,5 +1,6 @@
 package core.jdbc;
 
+import javax.annotation.Nullable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,9 +9,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcTemplate {
+    public <T> T queryForObject(String sql, RowMapper<T> mapper, PreparedStatementParameterSetter setter) {
+        List<T> list = this.queryForList(sql, mapper, setter);
+        return list.isEmpty() ? null : list.get(0);
+    }
+
     public <T> List<T> queryForList(String sql, RowMapper<T> mapper) {
+        return this.queryForList(sql, mapper, null);
+    }
+
+    public <T> List<T> queryForList(String sql, RowMapper<T> mapper, @Nullable PreparedStatementParameterSetter setter) {
         try (Connection con = ConnectionManager.getConnection();
              PreparedStatement pstmt = con.prepareStatement(sql)) {
+            if (setter != null) {
+                setter.set(pstmt);
+            }
+
             ResultSet rs = pstmt.executeQuery();
 
             List<T> list = new ArrayList<>();
@@ -20,24 +34,6 @@ public class JdbcTemplate {
             }
             rs.close();
             return list;
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
-    }
-
-    public <T> T queryForObject(String sql, RowMapper<T> mapper, PreparedStatementParameterSetter setter) {
-        try (Connection con = ConnectionManager.getConnection();
-             PreparedStatement pstmt = con.prepareStatement(sql)) {
-            setter.set(pstmt);
-
-            ResultSet rs = pstmt.executeQuery();
-
-            T mappedData = null;
-            if (rs.next()) {
-                mappedData = mapper.map(rs);
-            }
-            rs.close();
-            return mappedData;
         } catch (SQLException e) {
             throw new DataAccessException(e);
         }
