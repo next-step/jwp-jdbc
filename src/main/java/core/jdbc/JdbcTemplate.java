1fa95175719ceb8db0a392e1e8171c4b83f8b551
devson <sonyc5720@gmail.com>
2022-08-11 20:47:55 +0900

    feat: 복수의 User 객체 조회 기능을 제공하는 기능 생성

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -6,18 +6,36 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
-    public User queryForObject(String sql, RowMapper rowMapper, PreparedStatementParameterSetter setter) {
+    public List<User> queryForList(String sql, RowMapper rm) {
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+            ResultSet rs = pstmt.executeQuery();
+
+            List<User> users = new ArrayList<>();
+            while (rs.next()) {
+                User user = rm.map(rs);
+                users.add(user);
+            }
+            rs.close();
+            return users;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public User queryForObject(String sql, RowMapper mapper, PreparedStatementParameterSetter setter) {
         try (Connection con = ConnectionManager.getConnection();
              PreparedStatement pstmt = con.prepareStatement(sql)) {
             setter.set(pstmt);
-
             ResultSet rs = pstmt.executeQuery();
 
             User user = null;
             if (rs.next()) {
-                user = rowMapper.map(rs);
+                user = mapper.map(rs);
             }
             rs.close();
             return user;
--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -7,9 +7,7 @@ import next.model.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
@@ -49,29 +47,16 @@ public class UserDao {
 
     public List<User> findAll() {
         String sql = "SELECT userId, password, name, email FROM USERS";
-
-        try (Connection con = ConnectionManager.getConnection();
-             PreparedStatement pstmt = con.prepareStatement(sql)) {
-            ResultSet rs = pstmt.executeQuery();
-
-            ArrayList<User> users = new ArrayList<>();
-
-            while (rs.next()) {
-                User user = new User(
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        return jdbcTemplate.queryForList(
+                sql,
+                rs -> new User(
                         rs.getString("userId"),
                         rs.getString("password"),
                         rs.getString("name"),
                         rs.getString("email")
-                );
-                users.add(user);
-            }
-
-            rs.close();
-
-            return users;
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+                )
+        );
     }
 
     public User findByUserId(String userId) {
