6c8fd2c1b0024e63875a9a89c93ecccfc0b75a18
황정현 <jhhwang@wemakeprice.com>
2022-08-12 17:58:56 +0900

    feat JsonView 응답 구현

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,39 @@
 package core.mvc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.jfr.ContentType;
+import org.springframework.http.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 public class JsonView implements View {
+    private ObjectMapper mapper = new ObjectMapper();
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
+        if (model.size() > 0) {
+            response.getWriter().write(convertModel(model));
+        }
+    }
+
+    private String convertModel(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() > 1) {
+            return mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(model);
+        }
+
+        return mapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(model.get(getFirstKey(model)));
+    }
+
+    private String getFirstKey(Map<String, ?> model) {
+        return model.keySet()
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("모델이 비었습니다."));
     }
 }
---
+++ src/main/java/next/controller/Controller.java
@@ -0,0 +1,2 @@
+package next.controller;public class Controller {
+}
