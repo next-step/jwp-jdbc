31b2bdeabecf37ccda25d1642049851ba2f7fd62
devyonghee <devyonghee@gmail.com>
2022-08-14 00:28:37 +0900

    feat(JsonView.java): json 타입 모델 반환 기능 추가

--- src/test/java/core/mvc/JsonViewTest.java
+++ src/test/java/core/mvc/JsonViewTest.java
@@ -14,8 +14,8 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class JsonViewTest {
-    private static final Logger logger = LoggerFactory.getLogger( JsonViewTest.class );
+class JsonViewTest {
+    private static final Logger logger = LoggerFactory.getLogger(JsonViewTest.class);
     private MockHttpServletRequest request;
     private MockHttpServletResponse response;
     private View view;
