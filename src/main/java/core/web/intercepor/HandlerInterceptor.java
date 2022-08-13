56807497d72863d6a79ae73872f2ccd84a9c5723
seondongpyo <seondongpyo@gmail.com>
2022-08-13 12:10:32 +0900

    feat: HandlerInterceptor 인터페이스 추가

---
+++ src/main/java/core/web/intercepor/HandlerInterceptor.java
@@ -0,0 +1,19 @@
+package core.web.intercepor;
+
+import core.mvc.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface HandlerInterceptor {
+
+    default boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    default void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView mav) throws Exception {
+    }
+
+    default void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
+}
