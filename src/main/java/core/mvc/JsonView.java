4c086fc4616b8aefbe248016129e25f8c489d722
donghui.lee <donghui.lee@nhnsoft.com>
2022-08-15 19:24:53 +0900

    feat: 요구사항 1 구현

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,33 @@
 package core.mvc;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.MediaType;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JsonView implements View {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final String EMPTY_STRING = "";
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        PrintWriter writer = response.getWriter();
+        writer.write(valuesAsString(model));
+        writer.flush();
+    }
 
+    private String valuesAsString(Map<String, ?> model) throws JsonProcessingException {
+        if (model.size() == 1) {
+            return objectMapper.writeValueAsString(model.values().iterator().next());
+        }
+
+        return model.isEmpty() ? EMPTY_STRING : objectMapper.writeValueAsString(model);
     }
 }
