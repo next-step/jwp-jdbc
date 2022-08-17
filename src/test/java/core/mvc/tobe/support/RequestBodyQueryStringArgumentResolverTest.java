0e67217c2c33ac753abd457c93212de61aee202b
YONGJU <odetsuo@gmail.com>
2022-08-17 10:16:26 +0900

    refactor: ParameterizedTest로 변경
    동일한 테스트 코드의 true/false 검증이기에 ParameterizedTest로 변경하여
    테스트의 의도와 가독성 향상 목적

--- src/test/java/core/mvc/tobe/support/RequestBodyQueryStringArgumentResolverTest.java
+++ src/test/java/core/mvc/tobe/support/RequestBodyQueryStringArgumentResolverTest.java
@@ -1,42 +1,37 @@
 package core.mvc.tobe.support;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import core.mvc.tobe.MethodParameter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import org.assertj.core.api.Assertions;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class RequestBodyQueryStringArgumentResolverTest {
 
     final RequestBodyQueryStringArgumentResolver resolver = new RequestBodyQueryStringArgumentResolver();
 
-    @DisplayName("RequestBody 애노테이션이 없으면 변환을 할 수 없다")
-    @Test
-    void not_supports_without_request_body_annotation() throws NoSuchMethodException {
-
-        final Method method = MockArgumentResolverController.class.getDeclaredMethod("mockUser", MockUser.class);
+    @DisplayName("파라미터에 RequestBody 애노테이션의 존재 여부에 따라 변환할 수 있다")
+    @ParameterizedTest(name = "[{arguments}]")
+    @MethodSource
+    void supports_with_request_body_annotation(final String methodName, final boolean expected) throws NoSuchMethodException {
+        final Method method = MockArgumentResolverController.class.getDeclaredMethod(methodName, MockUser.class);
         final Annotation[][] parameterAnnotations = method.getParameterAnnotations();
         final MethodParameter methodParameter = new MethodParameter(method, MockUser.class, parameterAnnotations[0], "user");
 
         final boolean actual = resolver.supports(methodParameter);
 
-        assertThat(actual).isFalse();
+        assertThat(actual).isEqualTo(expected);
     }
 
-    @DisplayName("RequestBody 애노테이션이 있으면 변환 할 수 있다")
-    @Test
-    void supports_with_request_body_annotation() throws NoSuchMethodException {
-
-        final Method method = MockArgumentResolverController.class.getDeclaredMethod("mockRequestBodyUser", MockUser.class);
-        final Annotation[][] parameterAnnotations = method.getParameterAnnotations();
-        final MethodParameter methodParameter = new MethodParameter(method, MockUser.class, parameterAnnotations[0], "user");
-
-        final boolean actual = resolver.supports(methodParameter);
-
-        assertThat(actual).isFalse();
+    private static Stream<Arguments> supports_with_request_body_annotation() {
+        return Stream.of(
+            Arguments.of("mockUser", false),
+            Arguments.of("mockRequestBodyUser", true)
+        );
     }
 }
