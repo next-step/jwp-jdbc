b8235deb642cc961adf5fa6435c170c3ca616cd0
YONGJU <odetsuo@gmail.com>
2022-08-17 16:42:49 +0900

    refactor: RequestBodyArgumentResolver 상속 변경
    AbstractModelArgumentResolver을 상속하도록 변경함
    AbstractModelArgumentResolver에서 제공하는
    resolveArgument 메서드를 재사용하기 위한 목적

--- src/main/java/core/mvc/tobe/support/RequestBodyArgumentResolver.java
+++ src/main/java/core/mvc/tobe/support/RequestBodyArgumentResolver.java
@@ -2,18 +2,29 @@ package core.mvc.tobe.support;
 
 import core.annotation.web.RequestBody;
 import core.mvc.tobe.MethodParameter;
+import java.util.Arrays;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class RequestBodyArgumentResolver extends AbstractAnnotationArgumentResolver {
+public class RequestBodyArgumentResolver extends AbstractModelArgumentResolver {
 
     @Override
     public boolean supports(final MethodParameter methodParameter) {
-        return supportAnnotation(methodParameter, RequestBody.class);
+        return super.supports(methodParameter) && supportAnnotation(methodParameter);
+    }
+
+    private boolean supportAnnotation(MethodParameter methodParameter) {
+        return Arrays.stream(methodParameter.getAnnotations())
+            .anyMatch(ann -> ann.annotationType() == RequestBody.class);
     }
 
     @Override
     public Object resolveArgument(final MethodParameter methodParameter, final HttpServletRequest request, final HttpServletResponse response) {
+        return null;
+    }
+
+    @Override
+    String getParameter(final HttpServletRequest request, final String parameterName) {
         return null;
     }
 }
