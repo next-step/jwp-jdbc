54f9c2f354af8fe7fa153fb2b769854d632c4a42
HyungjuJang <dacapolife87@gmail.com>
2022-08-23 09:51:52 +0900

    feat : JdbcTemplate 구현

---
+++ src/main/java/next/dao/DataAccessException.java
@@ -0,0 +1,7 @@
+package next.dao;
+
+public class DataAccessException extends RuntimeException {
+    public DataAccessException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/next/dao/JdbcTemplate.java
@@ -0,0 +1,70 @@
+package next.dao;
+
+import core.jdbc.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcTemplate {
+
+    public void update(String sql, Object... parameters) throws DataAccessException {
+        update(sql, createPreparedStatementSetter(parameters));
+    }
+
+    public void update(String sql, PreparedStatementSetter pss) throws DataAccessException {
+        try (Connection con = ConnectionManager.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql);) {
+            pss.setValues(pstmt);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public PreparedStatementSetter createPreparedStatementSetter(Object... parameters) {
+        return pstmt -> {
+            for (int i = 0; i < parameters.length; i++) {
+                pstmt.setObject(i + 1, parameters[i]);
+
+            }
+        };
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters) throws DataAccessException {
+        return query(sql, rowMapper, createPreparedStatementSetter(parameters));
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, PreparedStatementSetter pss) throws DataAccessException {
+        ResultSet rs = null;
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql);) {
+            pss.setValues(pstmt);
+
+            rs = pstmt.executeQuery();
+
+            List<T> result = new ArrayList<T>();
+            if (rs.next()) {
+                result.add(rowMapper.mapRow(rs));
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T>T queryForObject(String sql, RowMapper<T> rowMapper, Object... parameters) throws DataAccessException {
+        return queryForObject(sql, rowMapper, createPreparedStatementSetter(parameters));
+    }
+
+    public <T>T queryForObject(String sql, RowMapper<T> rowMapper, PreparedStatementSetter pss) throws DataAccessException {
+        List<T> result = query(sql, rowMapper, pss);
+        if (result.isEmpty()) {
+            return null;
+        }
+
+        return result.get(0);
+    }
+}
