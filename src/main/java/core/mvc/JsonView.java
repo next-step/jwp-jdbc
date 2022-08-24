c2a6a13c33f1a65b8c07dbf953f3e23d9dcffc09
youngsuseo <56705264+youngsuseo@users.noreply.github.com>
2022-08-24 22:50:20 +0900

    feat (1-2. pass render_one_element method test) : pass render_one_element method test
    * JsonView: 모델안의 데이터가 하나일 때, 응답 body에 model 데이터를 담아 전달한다.

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,5 +1,6 @@
 package core.mvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,8 +8,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 public class JsonView implements View {
+    private static final int ONE_MODEL_COUNT = 1;
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String value = null;
+        if (model.size() == ONE_MODEL_COUNT) {
+            for (String key : model.keySet()) {
+                Object modelValue = model.get(key);
+                value = objectMapper.writeValueAsString(modelValue);
+            }
+        }
+
+        if (value != null) {
+            response.getWriter().write(value);
+        }
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
     }
 }
