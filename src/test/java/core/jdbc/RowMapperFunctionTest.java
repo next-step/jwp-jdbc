517d9422be6d03d84f8609a359e47db3dc3492fe
YONGJU <odetsuo@gmail.com>
2022-08-24 15:08:45 +0900

    refactor: RowMapperFunctionTest 에서 MockResultSet 적용
    ResultSet 인터페이스를 일부 메서드를 구현하는 MockResultSet을 추가하여
    Connection, Statement 를 사용하지 않고 ResultSet을 활용한 단위 테스트로 수정

--- src/test/java/core/jdbc/RowMapperFunctionTest.java
+++ src/test/java/core/jdbc/RowMapperFunctionTest.java
@@ -2,52 +2,30 @@ package core.jdbc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
 import next.model.User;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-class RowMapperFunctionTest extends TestDatabaseSetup {
-
-    private Connection connection;
-    private PreparedStatement preparedStatement;
-    private ResultSet resultSet;
-
-    @BeforeEach
-    void setUp() throws SQLException {
-        super.setup();
-
-        final String sql = "SELECT userId, password, name, email FROM USERS WHERE userId = ? and email = ?";
-        connection = ConnectionManager.getConnection();
-        preparedStatement = connection.prepareStatement(sql);
-    }
-
-    @AfterEach
-    void tearDown() throws SQLException {
-        preparedStatement.close();
-        connection.close();
-        resultSet.close();
-    }
+class RowMapperFunctionTest {
 
     @DisplayName("ResultSet 을 원하는 User 타입으로 반환한다")
     @Test
     void return_to_the_user_type() throws SQLException {
         // given
-        preparedStatement.setString(1, "admin");
-        preparedStatement.setString(2, "admin@slipp.net");
-
-        resultSet = preparedStatement.executeQuery();
+        final MockResultSet mockResultSet = createMockResultSet();
 
         final RowMapperFunction<Optional<User>> function = (rs) -> {
             if (rs.next()) {
@@ -64,7 +42,7 @@ class RowMapperFunctionTest extends TestDatabaseSetup {
         final User expected = new User("admin", "password", "자바지기", "admin@slipp.net");
 
         // when
-        final Optional<User> actual = function.apply(resultSet);
+        final Optional<User> actual = function.apply(mockResultSet);
 
         // then
         assertThat(actual).isPresent()
@@ -72,18 +50,10 @@ class RowMapperFunctionTest extends TestDatabaseSetup {
     }
 
     @DisplayName("ResultSet 을 String 타입으로 반환한다")
-    @ParameterizedTest(name = "userId {0} is {1}")
-    @CsvSource(value = {
-        "admin, admin",
-        "guest, null",
-    }, nullValues = "null")
-    void return_to_the_string_type(String userId, String expected) throws SQLException {
+    @ParameterizedTest(name = "data {0} is {1}")
+    @MethodSource
+    void return_to_the_string_type(ResultSet resultSet, String expected) throws SQLException {
         // given
-        preparedStatement.setString(1, userId);
-        preparedStatement.setString(2, "admin@slipp.net");
-
-        resultSet = preparedStatement.executeQuery();
-
         final RowMapperFunction<String> function = (rs) -> {
             if (rs.next()) {
                 return rs.getString("userId");
@@ -98,13 +68,20 @@ class RowMapperFunctionTest extends TestDatabaseSetup {
         assertThat(actual).isEqualTo(expected);
     }
 
+    private static Stream<Arguments> return_to_the_string_type() {
+        return Stream.of(
+            Arguments.of(createMockResultSet(), "admin"),
+            Arguments.of(new MockResultSet(Collections.emptyList()), null)
+        );
+    }
+
     @DisplayName("ResultSet 을 List<String> 타입으로 반환한다")
     @Test
     void return_to_the_list_type() throws SQLException {
         // given
-        connection.prepareStatement("INSERT INTO USERS VALUES ('tester', 'password', 'name', 'email')").executeUpdate();
-
-        resultSet = connection.prepareStatement("SELECT * FROM USERS").executeQuery();
+        Map<String, Object> secondRow = new HashMap<>();
+        secondRow.put("userId", "tester");
+        final MockResultSet resultSet = createMockResultSet(secondRow);
 
         final RowMapperFunction<List<String>> function = (rs) -> {
             List<String> userIds = new ArrayList<>();
@@ -121,5 +98,24 @@ class RowMapperFunctionTest extends TestDatabaseSetup {
 
         // then
         assertThat(actual).isEqualTo(expected);
+    }
+
+
+    private static MockResultSet createMockResultSet() {
+        return createMockResultSet(null);
+    }
+
+    private static MockResultSet createMockResultSet(Map<String, Object> newRow) {
+        List<Map<String, Object>> resultRows = new ArrayList<>();
+        Map<String, Object> resultRow = new HashMap<>();
+        resultRow.put("userId", "admin");
+        resultRow.put("password", "password");
+        resultRow.put("name", "자바지기");
+        resultRow.put("email", "admin@slipp.net");
+        resultRows.add(resultRow);
+        if (newRow != null && !newRow.isEmpty()) {
+            resultRows.add(newRow);
+        }
+        return new MockResultSet(resultRows);
     }
 }
