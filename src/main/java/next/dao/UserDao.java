55a17e78559cba285f0abeca5dc4dc69f9ba3544
mintjordy <mintjordy@woowahan.com>
2022-08-25 17:38:28 +0900

    refactor: 회원 업데이트 기능 jdbcTemplate 적용

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -26,29 +26,17 @@ public class UserDao {
         });
     }
 
-    public void update(User user) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
+    public void update(User user) {
+        jdbcTemplate.update(connection -> {
             String sql = "UPDATE USERS SET password=?," +
                     "name=?, email=? WHERE userId=?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, user.getPassword());
-            pstmt.setString(2, user.getName());
-            pstmt.setString(3, user.getEmail());
-            pstmt.setString(4, user.getUserId());
-
-            pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
-        }
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, user.getPassword());
+            preparedStatement.setString(2, user.getName());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setString(4, user.getUserId());
+            return preparedStatement;
+        });
     }
 
     public List<User> findAll() throws SQLException {
