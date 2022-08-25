c8734d6bf081eb6d8e0c1fd56b40f6cae30beed2
mintjordy <mintjordy@woowahan.com>
2022-08-25 17:53:40 +0900

    feat: 단건 Object 조회 기능 추가
    - 조회 결과가 0개, 혹은 1개보다 많은 경우 RuntimeException 발생

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -2,6 +2,7 @@ package core.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcTemplate {
@@ -9,6 +10,28 @@ public class JdbcTemplate {
         try (Connection connection = ConnectionManager.getConnection();
              PreparedStatement preparedStatement = preparedStatementCreator.createPreparedStatement(connection)) {
             preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new QueryExecuteFailException("Query 수행중 에러 발생 - " + e.getCause().getMessage(), e);
+        }
+    }
+
+    public <T> T queryForObject(PreparedStatementCreator preparedStatementCreator, RowMapper<T> rowMapper) {
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement preparedStatement = preparedStatementCreator.createPreparedStatement(connection)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            T object;
+            if (!resultSet.next()) {
+                throw new RuntimeException("Query 결과가 존재하지 않습니다.");
+            }
+
+            object = rowMapper.mapRow(resultSet);
+
+            if (resultSet.next()) {
+                throw new RuntimeException("Query 결과가 1개보다 많습니다.");
+            }
+
+            return object;
         } catch (SQLException e) {
             throw new QueryExecuteFailException("Query 수행중 에러 발생 - " + e.getCause().getMessage(), e);
         }
