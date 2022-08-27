29547f47b83d2b5bd4a4936ba5a6d786df70346f
Jeongsu Yang <mycat83@gmail.com>
2022-08-27 22:36:29 +0900

    feat: JsonView 구현

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,42 @@
 package core.mvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 
 public class JsonView implements View {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final int SINGLE_MODEL = 1;
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
+        if (model.isEmpty()) {
+            return;
+        }
+
+        if (model.size() == SINGLE_MODEL) {
+            renderSingleModel(model, response);
+            return;
+        }
+
+        renderMultipleModels(model, response);
+    }
+
+    private void renderSingleModel(Map<String, ?> model, HttpServletResponse response) throws IOException {
+        final Object value = model.values()
+                .stream()
+                .findFirst()
+                .orElse(null);
+        OBJECT_MAPPER.writeValue(response.getOutputStream(), value);
+    }
+
+    private void renderMultipleModels(Map<String, ?> model, HttpServletResponse response) throws IOException {
+        OBJECT_MAPPER.writeValue(response.getOutputStream(), model);
     }
 }
