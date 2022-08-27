3aca0828d50d90febf588ea10676a4c32593ad22
hongbin <fusis1@naver.com>
2022-08-27 12:40:21 +0900

    feat: RestAPI 컨트롤러 구현

--- src/main/java/core/mvc/ModelAndView.java
+++ src/main/java/core/mvc/ModelAndView.java
@@ -15,6 +15,10 @@ public class ModelAndView {
         this.view = view;
     }
 
+    public static ModelAndView JsonView() {
+        return new ModelAndView(new JsonView());
+    }
+
     public ModelAndView addObject(String attributeName, Object attributeValue) {
         model.put(attributeName, attributeValue);
         return this;
---
+++ src/main/java/next/controller/UserApiController.java
@@ -0,0 +1,65 @@
+package next.controller;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import core.annotation.web.Controller;
+import core.annotation.web.PathVariable;
+import core.annotation.web.RequestMapping;
+import core.annotation.web.RequestMethod;
+import core.mvc.JsonUtils;
+import core.mvc.ModelAndView;
+import next.dto.UserCreatedDto;
+import next.dto.UserUpdatedDto;
+import next.model.User;
+
+@Controller
+public class UserApiController {
+
+    private static final Map<String, User> USERS = new ConcurrentHashMap<>();
+
+    // 회원가입
+    @RequestMapping(value = "/api/users", method = RequestMethod.POST)
+    public ModelAndView create(
+        HttpServletRequest request,
+        HttpServletResponse response) throws Exception {
+
+        UserCreatedDto userCreatedDto = JsonUtils.toObject(request.getInputStream(), UserCreatedDto.class);
+
+        var user = userCreatedDto.toUser();
+        USERS.put(user.getUserId(), user);
+
+        response.setStatus(HttpServletResponse.SC_CREATED);
+        response.setHeader("Location", "/api/users/" + user.getUserId());
+
+        return ModelAndView.JsonView();
+    }
+
+    // 조회
+    @RequestMapping(value = "/api/users/{id}", method = RequestMethod.GET)
+    public ModelAndView get(@PathVariable String id) {
+        var user = USERS.get(id);
+
+        var modelAndView = ModelAndView.JsonView();
+        modelAndView.addObject("userId", user.getUserId());
+        modelAndView.addObject("name", user.getName());
+        modelAndView.addObject("email", user.getEmail());
+
+        return modelAndView;
+    }
+
+    // 수정
+    @RequestMapping(value = "/api/users/{id}", method = RequestMethod.PUT)
+    public ModelAndView modify(HttpServletRequest request, HttpServletResponse response, @PathVariable String id) throws
+        Exception {
+        UserUpdatedDto userUpdatedDto = JsonUtils.toObject(request.getInputStream(), UserUpdatedDto.class);
+
+        var user = USERS.get(id);
+        user.modify(userUpdatedDto.getName(), userUpdatedDto.getEmail());
+
+        return ModelAndView.JsonView();
+    }
+}
