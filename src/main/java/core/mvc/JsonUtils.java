4f284605cfe0d66d2658d61db5f8b0b7a0a3f52d
hongbin <fusis1@naver.com>
2022-08-27 12:39:58 +0900

    feat: 1단계 미션 구현
     - JsonView 구현

--- src/main/java/core/mvc/JsonUtils.java
+++ src/main/java/core/mvc/JsonUtils.java
@@ -1,21 +1,43 @@
 package core.mvc;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class JsonUtils {
-    public static <T> T toObject(String json, Class<T> clazz) throws ObjectMapperException {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    public static <T> T toObject(InputStream inputStream, Class<T> clazz) throws ObjectMapperException {
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.setVisibility(objectMapper.getSerializationConfig().getDefaultVisibilityChecker()
-                    .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
-            return objectMapper.readValue(json, clazz);
+            return OBJECT_MAPPER.readValue(inputStream, clazz);
         } catch (IOException e) {
             throw new ObjectMapperException(e);
         }
+    }
+
+    public static <T> T toObject(String json, Class<T> clazz) throws ObjectMapperException {
+        try {
+            OBJECT_MAPPER.setVisibility(OBJECT_MAPPER.getSerializationConfig().getDefaultVisibilityChecker()
+                    .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+                    .withGetterVisibility(JsonAutoDetect.Visibility.ANY)
+                    .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
+
+            var jsonNode = OBJECT_MAPPER.readTree(json);
+            var path = jsonNode.path(clazz.getSimpleName().toLowerCase());
+            var target = path.toString();
+
+            return OBJECT_MAPPER.readValue(target, clazz);
+        } catch (IOException e) {
+            throw new ObjectMapperException(e);
+        }
+    }
+
+    public static String toJson(Object obj) throws JsonProcessingException {
+        return OBJECT_MAPPER.writeValueAsString(obj);
     }
 }
