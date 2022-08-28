0f5a3f5a984cc92dfb2c8dc9f77d8d50448a774c
fistkim101 <fistkim101@gmail.com>
2022-08-28 23:12:40 +0900

    feat : JsonView 추가

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,30 @@
 package core.mvc;
 
+import next.support.ObjectMapperFactory;
+import org.springframework.http.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 public class JsonView implements View {
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
+        if (model.size() == 1) {
+            ObjectMapperFactory.getInstance().writeValue(response.getOutputStream(), this.getSingleData(model));
+            return;
+        }
+
+        ObjectMapperFactory.getInstance().writeValue(response.getOutputStream(), model);
     }
+
+    private Object getSingleData(Map<String, ?> model) {
+        return model.values().stream()
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
 }
