35b963515a763fba024a6d5b0a97c2757e2a040f
JeongWan Gho <rhwjddhks@gmail.com>
2022-08-28 20:13:57 +0900

    feat: UserDaoTest 를 통과하도록 UserDao 구현

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -36,12 +36,41 @@ public class UserDao {
     }
 
     public void update(User user) throws SQLException {
-        // TODO 구현 필요함.
+        String sql = "UPDATE USERS SET password = ?, name = ?, email = ? WHERE userId = ?";
+        try (
+                Connection con = ConnectionManager.getConnection();
+                PreparedStatement pstmt = con.prepareStatement(sql)
+        ) {
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
+
+            pstmt.executeUpdate();
+        }
     }
 
     public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        return new ArrayList<User>();
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT userId, password, name, email FROM USERS";
+        try (
+                Connection con = ConnectionManager.getConnection();
+                PreparedStatement pstmt = con.prepareStatement(sql)
+        ) {
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    users.add(
+                            new User(
+                                    rs.getString("userId"),
+                                    rs.getString("password"),
+                                    rs.getString("name"),
+                                    rs.getString("email")
+                            )
+                    );
+                }
+            }
+        }
+        return users;
     }
 
     public User findByUserId(String userId) throws SQLException {
