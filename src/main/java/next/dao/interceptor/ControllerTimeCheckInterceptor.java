77711a29f83e199bd4ecdae9cc748cb423268bb0
황정현 <jhhwang@wemakeprice.com>
2022-08-29 12:36:59 +0900

    refactor 메소드 실행속도 구하기 로직 변경

--- src/main/java/next/dao/interceptor/ControllerTimeCheckInterceptor.java
+++ src/main/java/next/dao/interceptor/ControllerTimeCheckInterceptor.java
@@ -5,24 +5,26 @@ import core.mvc.ModelAndView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;}
 import javax.servlet.http.HttpServletResponse;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class ControllerTimeCheckInterceptor implements HandlerInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(ControllerTimeCheckInterceptor.class);
-    private long startTime;
-    private long endTime;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        startTime = System.currentTimeMillis();
+        LocalDateTime startTime = LocalDateTime.now();
+        request.setAttribute("startTime", startTime);
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
-        endTime = System.currentTimeMillis();
-        logger.debug("메소드 실행속도 {} millis", endTime - startTime);
+        LocalDateTime startTime = (LocalDateTime) request.getAttribute("startTime");
+        LocalDateTime endTime = LocalDateTime.now();
+        logger.debug("메소드 실행속도 {} millis", Duration.between(startTime, endTime));
     }
 
     @Override
---
+++ src/test/java/next/dao/interceptor/HandlerInterceptorCompositeTest.java
@@ -0,0 +1,17 @@
+package next.dao.interceptor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTimeCheckInterceptorTest {
+
+    @Test
+    void preHandle() {
+
+    }
+
+    @Test
+    void postHandle() {
+    }
+}
