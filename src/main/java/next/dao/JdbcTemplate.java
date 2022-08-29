1d0de04671f70e2bec993c824c67e14040947f60
Jeongsu Yang <mycat83@gmail.com>
2022-08-29 23:54:24 +0900

    refactor: JDBC 라이브러리 리스트 조회 구현

--- src/main/java/next/dao/JdbcTemplate.java
+++ src/main/java/next/dao/JdbcTemplate.java
@@ -9,6 +9,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
     public void update(String sql, Object... params) {
@@ -68,6 +70,48 @@ public class JdbcTemplate {
             }
         }
         return instance;
+    }
+
+    public <T> List<T> selectList(String sql, Class<T> returnType, Object... params) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setString(i + 1, String.valueOf(params[i]));
+            }
+
+            rs = pstmt.executeQuery();
+            return extractList(returnType, rs);
+        } catch (SQLException e) {
+            throw new IllegalStateException("쿼리 실행을 실패하였습니다.", e);
+        } catch (InvocationTargetException | NoSuchMethodException | InstantiationException |
+                 IllegalAccessException e) {
+            throw new IllegalStateException("쿼리 결과 변환을 실패하였습니다.", e);
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+
+    private <T> List<T> extractList(Class<T> returnType, ResultSet rs)
+            throws NoSuchMethodException, SQLException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        final Field[] fields = returnType.getDeclaredFields();
+        final Constructor<T> constructor = returnType.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        final List<T> list = new ArrayList<>();
+
+        while (rs.next()) {
+            final T instance = constructor.newInstance();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                field.set(instance, rs.getString(field.getName()));
+            }
+            list.add(instance);
+        }
+        return list;
     }
 
     private void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -1,13 +1,7 @@
 package next.dao;
 
-import core.jdbc.ConnectionManager;
 import next.model.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
@@ -27,39 +21,19 @@ public class UserDao {
         );
     }
 
-    public List<User> findAll() throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS";
-            pstmt = con.prepareStatement(sql);
-            rs = pstmt.executeQuery();
-
-            ArrayList<User> users = new ArrayList<>();
-            while (rs.next()) {
-                users.add(new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email")));
-            }
-            return users;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+    public List<User> findAll() {
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        return jdbcTemplate.selectList(
+                "SELECT userId, password, name, email FROM USERS",
+                User.class
+        );
     }
 
     public User findByUserId(String userId) {
         final JdbcTemplate jdbcTemplate = new JdbcTemplate();
         return jdbcTemplate.selectObject(
                 "SELECT userId, password, name, email FROM USERS WHERE userid = ?",
-                User.class, userId);
+                User.class, userId
+        );
     }
 }
