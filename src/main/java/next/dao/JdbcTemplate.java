908a326be33ada25a14ccc93afeeeeb3d4fdb439
Jeongsu Yang <mycat83@gmail.com>
2022-08-29 23:46:43 +0900

    refactor: JDBC 라이브러리 한건 조회 구현

---
+++ src/main/java/next/dao/JdbcTemplate.java
@@ -0,0 +1,98 @@
+package next.dao;
+
+import core.jdbc.ConnectionManager;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcTemplate {
+    public void update(String sql, Object... params) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setString(i + 1, String.valueOf(params[i]));
+            }
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new IllegalStateException("쿼리 실행을 실패하였습니다.", e);
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    public <T> T selectObject(String sql, Class<T> returnType, Object... params) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setString(i + 1, String.valueOf(params[i]));
+            }
+
+            rs = pstmt.executeQuery();
+            return extractObject(rs, returnType);
+        } catch (SQLException e) {
+            throw new IllegalStateException("쿼리 실행을 실패하였습니다.", e);
+        } catch (InvocationTargetException | NoSuchMethodException | InstantiationException |
+                 IllegalAccessException e) {
+            throw new IllegalStateException("쿼리 결과 변환을 실패하였습니다.", e);
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+
+    private <T> T extractObject(ResultSet rs, Class<T> returnType)
+            throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException, SQLException {
+        final Field[] fields = returnType.getDeclaredFields();
+        final Constructor<T> constructor = returnType.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        final T instance = constructor.newInstance();
+
+        if (rs.next()) {
+            for (Field field : fields) {
+                field.setAccessible(true);
+                field.set(instance, rs.getString(field.getName()));
+            }
+        }
+        return instance;
+    }
+
+    private void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new IllegalStateException("ReseltSet 닫기를 실패하였습니다.", e);
+            }
+        }
+
+        if (pstmt != null) {
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+                throw new IllegalStateException("PreparedStatement 닫기를 실패하였습니다.", e);
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new IllegalStateException("Connection 닫기를 실패하였습니다.", e);
+            }
+        }
+    }
+}
--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -11,51 +11,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
-    public void insert(User user) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, user.getUserId());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getName());
-            pstmt.setString(4, user.getEmail());
-
-            pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
-        }
+    public void insert(User user) {
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.update(
+                "INSERT INTO USERS (userId, password, name, email) VALUES (?, ?, ?, ?)",
+                user.getUserId(), user.getPassword(), user.getName(), user.getEmail()
+        );
     }
 
-    public void update(User user) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "UPDATE USERS SET name = ?, email = ? WHERE userId = ?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, user.getName());
-            pstmt.setString(2, user.getEmail());
-            pstmt.setString(3, user.getUserId());
-
-            pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
-        }
+    public void update(User user) {
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.update(
+                "UPDATE USERS SET name = ?, email = ? WHERE userId = ?",
+                user.getName(), user.getEmail(), user.getUserId()
+        );
     }
 
     public List<User> findAll() throws SQLException {
@@ -87,35 +56,10 @@ public class UserDao {
         }
     }
 
-    public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid = ?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
-
-            rs = pstmt.executeQuery();
-
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-            }
-
-            return user;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+    public User findByUserId(String userId) {
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        return jdbcTemplate.selectObject(
+                "SELECT userId, password, name, email FROM USERS WHERE userid = ?",
+                User.class, userId);
     }
 }
