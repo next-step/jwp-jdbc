3db1dbb18c97621d6c5767677e3d9f4ab15d4eda
황정현 <jhhwang@wemakeprice.com>
2022-08-29 12:37:17 +0900

    feat 테스트코드 추가

--- src/test/java/next/dao/interceptor/HandlerInterceptorCompositeTest.java
+++ src/test/java/next/dao/interceptor/HandlerInterceptorCompositeTest.java
@@ -1,17 +1,35 @@
 package next.dao.interceptor;
 
+import next.controller.HomeController;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ControllerTimeCheckInterceptorTest {
+class HandlerInterceptorCompositeTest {
+
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+    private HandlerInterceptorComposite handlerInterceptorComposite;
+
+    @BeforeEach
+    void setup() {
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+        handlerInterceptorComposite = HandlerInterceptorComposite.getInstance();
+    }
 
     @Test
     void preHandle() {
-
+        boolean preHandle = handlerInterceptorComposite.preHandle(request, response, new HomeController());
+        Assertions.assertThat(preHandle).isTrue();
     }
 
     @Test
     void postHandle() {
+        handlerInterceptorComposite.preHandle(request, response, new HomeController());
     }
 }
