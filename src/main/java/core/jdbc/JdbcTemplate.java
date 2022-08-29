f8d3b98c8e21dd4019effe60d254ab919b7b601d
mintjordy <mintjordy@woowahan.com>
2022-08-30 00:06:24 +0900

    refactor: 리뷰반영 - PreparedStatement 매개변수 세팅 로직을 람다식 분리

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -23,6 +23,15 @@ public class JdbcTemplate {
         }
     }
 
+    public void update(String sql, PreparedStatementSetter preparedStatementSetter) {
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement preparedStatement = createPreparedStatement(connection, sql, preparedStatementSetter)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new QueryExecuteFailException(QUERY_EXECUTE_FAIL_EXCEPTION_MESSAGE_PREFIX + e.getCause().getMessage(), e);
+        }
+    }
+
     public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... parameters) {
         try (Connection connection = ConnectionManager.getConnection();
              PreparedStatement preparedStatement = createPreparedStatement(connection, sql, parameters);
@@ -57,6 +66,12 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             throw new QueryExecuteFailException(QUERY_EXECUTE_FAIL_EXCEPTION_MESSAGE_PREFIX + e.getCause().getMessage(), e);
         }
+    }
+
+    private PreparedStatement createPreparedStatement(Connection connection, String sql, PreparedStatementSetter preparedStatementSetter) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatementSetter.setValues(preparedStatement);
+        return preparedStatement;
     }
 
     private PreparedStatement createPreparedStatement(Connection connection, String sql, Object... parameters) throws SQLException {
