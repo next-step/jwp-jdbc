1f31ff4f63d6bd3cc50a2469b86414b479d483d8
wu2ee <gulb1602@gmail.com>
2022-08-30 20:47:18 +0900

    feat : JsonView 의 render 메서드 구현

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,40 @@
 package core.mvc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
+    private static final int SINGLE_SIZE = 1;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        if (model.isEmpty()) {
+            return;
+        }
+        try (PrintWriter responseWriter = response.getWriter()) {
+            String jsonString = getJsonString(model);
+            responseWriter.write(jsonString);
+        }
+    }
 
+    private String getJsonString(Map<String, ?> model) throws JsonProcessingException {
+        if (isSingleModel(model)) {
+            Object modelValue = model.values().stream()
+                    .findFirst().get();
+            return objectMapper.writeValueAsString(modelValue);
+        }
+        return objectMapper.writeValueAsString(model);
+    }
+
+    private boolean isSingleModel(Map<String, ?> model) {
+        return model.size() == SINGLE_SIZE;
     }
 }
--- src/test/java/core/mvc/JsonViewTest.java
+++ src/test/java/core/mvc/JsonViewTest.java
@@ -1,6 +1,7 @@
 package core.mvc;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +29,7 @@ public class JsonViewTest {
     }
 
     @Test
+    @DisplayName("model 이 비어있는 경우의 render 를 진행한다.")
     void render_no_element() throws Exception {
         view.render(new HashMap<>(), request, response);
         assertThat(response.getContentType()).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
@@ -35,6 +37,7 @@ public class JsonViewTest {
     }
 
     @Test
+    @DisplayName("model 이 1개인 경우의 render 를 진행한다.")
     void render_one_element() throws Exception {
         Map<String, Object> model = new HashMap<>();
         Car expected = new Car("Black", "Sonata");
@@ -48,6 +51,7 @@ public class JsonViewTest {
     }
 
     @Test
+    @DisplayName("model 이 2개 이상인 경우의 render 를 진행한다.")
     void render_over_two_element() throws Exception {
         Map<String, Object> model = new HashMap<>();
         Car expected = new Car("Black", "Sonata");
