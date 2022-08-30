fc11b45b3347bb9a3822b83e6b214961439bdc06
Jeongsu Yang <mycat83@gmail.com>
2022-08-30 13:36:45 +0900

    2단계 - JDBC 라이브러리 구현 (#160)
    * refactor: 메서드 추출
    
    * feat: UserDao 구현
    
    * refactor: JDBC 라이브러리 한건 조회 구현
    
    * refactor: JDBC 라이브러리 리스트 조회 구현
    
    * refactor: try-with-resources 적용
    
    * refactor: mapParams 메서드 추출

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -1,78 +1,39 @@
 package next.dao;
 
-import core.jdbc.ConnectionManager;
 import next.model.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
-    public void insert(User user) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, user.getUserId());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getName());
-            pstmt.setString(4, user.getEmail());
-
-            pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
-        }
+    public void insert(User user) {
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.update(
+                "INSERT INTO USERS (userId, password, name, email) VALUES (?, ?, ?, ?)",
+                user.getUserId(), user.getPassword(), user.getName(), user.getEmail()
+        );
     }
 
-    public void update(User user) throws SQLException {
-        // TODO 구현 필요함.
+    public void update(User user) {
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.update(
+                "UPDATE USERS SET name = ?, email = ? WHERE userId = ?",
+                user.getName(), user.getEmail(), user.getUserId()
+        );
     }
 
-    public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        return new ArrayList<User>();
+    public List<User> findAll() {
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        return jdbcTemplate.selectList(
+                "SELECT userId, password, name, email FROM USERS",
+                User.class
+        );
     }
 
-    public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
-
-            rs = pstmt.executeQuery();
-
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-            }
-
-            return user;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+    public User findByUserId(String userId) {
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        return jdbcTemplate.selectObject(
+                "SELECT userId, password, name, email FROM USERS WHERE userid = ?",
+                User.class, userId
+        );
     }
 }
