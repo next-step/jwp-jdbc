b30d2e11844be33320f8197ebaa37c822e9d0828
Jeongsu Yang <mycat83@gmail.com>
2022-09-04 00:17:16 +0900

    refactor: try-with-resources 구문 적용

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -9,83 +9,52 @@ import java.util.List;
 
 public class JdbcTemplate {
     public void update(String sql, PreparedStatementSetter pstmtSetter) {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
             pstmtSetter.setValues(pstmt);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new DataAccessException("update 실행을 실패하였습니다.", e);
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
         }
     }
-    public List<Object> query(String sql, PreparedStatementSetter pstmtSetter, RowMapper rowMapper) {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
-            pstmtSetter.setValues(pstmt);
-            rs = pstmt.executeQuery();
 
-            List<Object> list = new ArrayList<>();
+    public List<Object> query(String sql, PreparedStatementSetter pstmtSetter, RowMapper rowMapper) {
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmtSetter.setValues(pstmt);
+            return convertToList(rowMapper, pstmt);
+        } catch (SQLException e) {
+            throw new DataAccessException("query 실행을 실패하였습니다.", e);
+        }
+    }
+
+    private List<Object> convertToList(RowMapper rowMapper, PreparedStatement pstmt) throws SQLException {
+        List<Object> list = new ArrayList<>();
+        try (ResultSet rs = pstmt.executeQuery()) {
             while (rs.next()) {
                 list.add(rowMapper.mapRow(rs));
             }
-            return list;
-        } catch (SQLException e) {
-            throw new DataAccessException("query 실행을 실패하였습니다.", e);
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
         }
+        return list;
     }
 
     public Object queryForObject(String sql, PreparedStatementSetter pstmtSetter, RowMapper rowMapper) {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
             pstmtSetter.setValues(pstmt);
-            rs = pstmt.executeQuery();
+            return convertToObject(rowMapper, pstmt);
+        } catch (SQLException e) {
+            throw new DataAccessException("queryForObject 실행을 실패하였습니다.", e);
+        }
+    }
 
-            Object o = null;
+    private Object convertToObject(RowMapper rowMapper, PreparedStatement pstmt) throws SQLException {
+        Object o = null;
+        try (ResultSet rs = pstmt.executeQuery()) {
             if (rs.next()) {
                 o = rowMapper.mapRow(rs);
             }
-
-            return o;
-        } catch (SQLException e) {
-            throw new DataAccessException("queryForObject 실행을 실패하였습니다.", e);
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
         }
+        return o;
     }
 }
