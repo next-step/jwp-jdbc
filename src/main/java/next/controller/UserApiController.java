48932c80b4414495430ce5eac477d8da3122b563
Jungsu Lee <lsecret@naver.com>
2022-09-05 10:16:46 +0900

    Step2 JDBC 라이브러리 구현 (#172)
    * refactor: function rename (코드 리뷰 반영)
    
    * docs: step2 기능 명세 정리
    
    * feat: UserDao 기능 구현
    
    * refactor: JdbcTemplate, RowMapper 생성을 통하여 공통 부분 리팩토링
    
    * refactor: step1 인수 테스트 DataBase 직접 사용 -> UserDao 사용으로 변경 (코드 리뷰 반영)

--- src/main/java/next/controller/UserApiController.java
+++ src/main/java/next/controller/UserApiController.java
@@ -6,9 +6,9 @@ import core.annotation.web.Controller;
 import core.annotation.web.RequestMapping;
 import core.annotation.web.RequestMethod;
 import core.annotation.web.RequestParam;
-import core.db.DataBase;
 import core.mvc.JsonView;
 import core.mvc.ModelAndView;
+import next.dao.UserDao;
 import next.dto.UserCreatedDto;
 import next.dto.UserUpdatedDto;
 import next.model.User;
@@ -18,10 +18,12 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class UserApiController {
 
+    private final UserDao userDao = new UserDao();
+
     @RequestMapping(value = "/api/users", method = RequestMethod.POST)
     public ModelAndView createUser(@RequestBody UserCreatedDto userCreatedDto, HttpServletResponse response) {
         User user = new User(userCreatedDto.getUserId(), userCreatedDto.getPassword(), userCreatedDto.getName(), userCreatedDto.getEmail());
-        DataBase.addUser(user);
+        userDao.insert(user);
 
         response.setStatus(HttpServletResponse.SC_CREATED);
         response.setHeader(HttpHeaders.LOCATION, "/api/users?userId=" + user.getUserId());
@@ -31,13 +33,14 @@ public class UserApiController {
 
     @RequestMapping(value = "/api/users", method = RequestMethod.GET)
     public ModelAndView findUser(@RequestParam String userId) {
-        return userModelAndView(DataBase.findUserById(userId));
+        return userModelAndView(userDao.findByUserId(userId));
     }
 
     @RequestMapping(value = "/api/users", method = RequestMethod.PUT)
     public ModelAndView updateUser(@RequestParam String userId, @RequestBody UserUpdatedDto userUpdatedDto) {
-        User user = DataBase.findUserById(userId);
+        User user = userDao.findByUserId(userId);
         user.update(userUpdatedDto.getName(), userUpdatedDto.getEmail());
+        userDao.update(user);
         return userModelAndView(user);
     }
 
