fa1d6f33559041155ccda43fc598f0eb7687b54d
wu2ee <gulb1602@gmail.com>
2022-09-06 02:34:15 +0900

    refactor : JdbcTemplate 콜백 패턴 구현 (함수형 인터페이스, 람다 사용)
    - 가변인자, PreparedStatementSetter 파라미터 타입에 따른 테스트 추가

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -9,14 +9,44 @@ import java.util.List;
 public class UserDao {
     private static final JdbcTemplate jdbcTemplate = JdbcTemplate.getInstance();
 
-    public void insert(User user) {
+    public void insert_varargs(User user) {
         jdbcTemplate.update("INSERT INTO USERS VALUES (?, ?, ?, ?)",
                 user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
     }
 
-    public void update(User user) {
+    public void insert_ps(User user) {
+        jdbcTemplate.update("INSERT INTO USERS VALUES (?, ?, ?, ?)", ps -> {
+                    ps.setString(1, user.getUserId());
+                    ps.setString(2, user.getPassword());
+                    ps.setString(3, user.getName());
+                    ps.setString(4, user.getEmail());
+                }
+        );
+    }
+
+    public void update_varargs(User user) {
         jdbcTemplate.update("UPDATE USERS SET password = ?, name = ?, email = ? WHERE userId = ?",
                 user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
+    }
+
+    public void update_ps(User user) {
+        jdbcTemplate.update("UPDATE USERS SET password = ?, name = ?, email = ? WHERE userId = ?", ps -> {
+                    ps.setString(1, user.getPassword());
+                    ps.setString(2, user.getName());
+                    ps.setString(3, user.getEmail());
+                    ps.setString(4, user.getUserId());
+                }
+        );
+    }
+
+    public User findByUserId_varargs(String userId) {
+        return jdbcTemplate.queryForObject("SELECT userId, password, name, email FROM USERS WHERE userId = ?",
+                new ObjectRowMapper<>(User.class), userId);
+    }
+
+    public User findByUserId_ps(String userId) {
+        return jdbcTemplate.queryForObject("SELECT userId, password, name, email FROM USERS WHERE userId = ?",
+                new ObjectRowMapper<>(User.class), ps -> ps.setString(1, userId));
     }
 
     public List<User> findAll() {
@@ -24,8 +54,13 @@ public class UserDao {
                 new ObjectRowMapper<>(User.class));
     }
 
-    public User findByUserId(String userId) {
-        return jdbcTemplate.queryForObject("SELECT userId, password, name, email FROM USERS WHERE userId = ?",
-                new ObjectRowMapper<>(User.class), userId);
+    public List<User> findAll_varargs(String name) {
+        return jdbcTemplate.query("SELECT userId, password, name, email FROM USERS WHERE name = ?",
+                new ObjectRowMapper<>(User.class), name);
+    }
+
+    public List<User> findAll_ps(String name) {
+        return jdbcTemplate.query("SELECT userId, password, name, email FROM USERS WHERE name = ?",
+                new ObjectRowMapper<>(User.class), ps -> ps.setString(1, name));
     }
 }
--- src/main/resources/jwp.sql
+++ src/main/resources/jwp.sql
@@ -10,6 +10,7 @@ CREATE TABLE USERS (
 );
 
 INSERT INTO USERS VALUES('admin', 'password', '자바지기', 'admin@slipp.net');
+INSERT INTO USERS VALUES('admin1', 'password1', '자바지기', 'admin1@slipp.net');
 
 DROP TABLE IF EXISTS QUESTIONS;
 
