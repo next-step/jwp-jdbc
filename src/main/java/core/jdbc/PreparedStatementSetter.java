98fee239e9e72b4b7764e23edeb59e8836c8808a
Shin seongsu <tlstjdtn321@naver.com>
2022-09-06 22:27:37 +0900

    🚀 2단계 - JDBC 라이브러리 구현 (#178)
    * docs: 기능 문서 작성
    
    * feat: JdbcTemplate 만들기
    
    * feat: jdbcTemplate으로 User관련 DB 작업 수정
    
    * test: 필요없는 throw 제거

---
+++ src/main/java/core/jdbc/PreparedStatementSetter.java
@@ -0,0 +1,10 @@
+package core.jdbc;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface PreparedStatementSetter {
+
+    void setValues(PreparedStatement ps) throws SQLException;
+}
