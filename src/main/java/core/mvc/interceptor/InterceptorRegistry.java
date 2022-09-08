330bb0e57631f70c99fb66dd4d9edf50f1b01cbe
hongbin <fusis1@naver.com>
2022-09-09 00:10:09 +0900

    fix: post interceptor 순서 변경
     - runnable 파라미터명 변경

--- src/main/java/core/mvc/interceptor/InterceptorRegistry.java
+++ src/main/java/core/mvc/interceptor/InterceptorRegistry.java
@@ -14,9 +14,9 @@ public class InterceptorRegistry {
         interceptors.add(interceptor);
     }
 
-    public void handle(Runnable runnable, HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public void handle(Runnable service, HttpServletRequest request, HttpServletResponse response, Object handler) {
         preHandle(request, response, handler);
-        runnable.run();
+        service.run();
         postHandle(request, response, handler);
     }
 
@@ -27,8 +27,8 @@ public class InterceptorRegistry {
     }
 
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        for (HandlerInterceptor interceptor : interceptors) {
-            interceptor.postHandle(request, response, handler);
+        for (int i = interceptors.size() - 1; i >= 0; i--) {
+            interceptors.get(i).postHandle(request, response, handler);
         }
     }
 }
