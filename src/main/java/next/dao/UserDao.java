e46946187824c1d7c4d145c0585a210e0621b736
Shin seongsu <tlstjdtn321@naver.com>
2022-09-08 20:19:46 +0900

    🚀 3단계 - JDBC 라이브러리 구현(힌트) (#182)
    * docs: 기능 문서 작성
    
    * feat: JdbcTemplate 만들기
    
    * feat: jdbcTemplate으로 User관련 DB 작업 수정
    
    * test: 필요없는 throw 제거
    
    * fix:
    1. ResultSet close 닫기
    2. 2개이상 조회시, 에러 추가
    
    * fix: 테스트를 위해 로그인 하는 쿼리 추가
    
    * test: 2개 이상 쿼리 조회 될시 에러 반환
    
    * fix:
    1. ResultSet close 닫기
    2. 2개이상 조회시, 에러 추가
    
    * fix: 테스트를 위해 로그인 하는 쿼리 추가
    
    * test: 2개 이상 쿼리 조회 될시 에러 반환

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -6,24 +6,30 @@ import java.util.List;
 
 public class UserDao {
 
-    public void insert(User user) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+    private JdbcTemplate jdbcTemplate;
 
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void insert(User user) {
         String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
         jdbcTemplate.update(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
     }
 
     public void update(User user) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate();
         String sql = "UPDATE USERS SET PASSWORD = ?, NAME = ?, EMAIL = ? WHERE USERID = ?";
-        jdbcTemplate.update(sql, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
+        jdbcTemplate.update(sql, pstmt -> {
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
+        });
     }
 
     public List<User> findAll() {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate();
-
         String sql = "SELECT userId, password, name, email FROM USERS";
-        return (List<User>)jdbcTemplate.query(sql, rs -> new User(
+        return jdbcTemplate.query(sql, rs -> new User(
             rs.getString("userId"),
             rs.getString("password"),
             rs.getString("name"),
@@ -31,7 +37,6 @@ public class UserDao {
     }
 
     public User findByUserId(String userId) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate();
         String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
         return jdbcTemplate.queryForObject(sql, rs -> new User(
             rs.getString("userId"),
@@ -39,4 +44,17 @@ public class UserDao {
             rs.getString("name"),
             rs.getString("email")), userId);
     }
+
+    public User findByEmailAndPassword(String email, String password) {
+        String sql = "SELECT userId, password, name, email FROM USERS WHERE email=? AND PASSWORD=? ";
+        return jdbcTemplate.queryForObject(sql, rs -> new User(
+            rs.getString("userId"),
+            rs.getString("password"),
+            rs.getString("name"),
+            rs.getString("email")), pstmt -> {
+            pstmt.setString(1, email);
+            pstmt.setString(2, password);
+        });
+    }
+
 }
--- src/test/java/next/dao/UserDaoTest.java
+++ src/test/java/next/dao/UserDaoTest.java
@@ -1,8 +1,11 @@
 package next.dao;
 
 import core.jdbc.ConnectionManager;
+import core.jdbc.JdbcTemplate;
+import core.jdbc.ManyResultQueryException;
 import next.model.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
@@ -11,20 +14,23 @@ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class UserDaoTest {
+
+    UserDao userDao;
 
     @BeforeEach
     public void setup() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScript(new ClassPathResource("jwp.sql"));
         DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+        this.userDao = new UserDao(new JdbcTemplate());
     }
 
     @Test
     public void crud() {
         User expected = new User("userId", "password", "name", "javajigi@email.com");
-        UserDao userDao = new UserDao();
         userDao.insert(expected);
         User actual = userDao.findByUserId(expected.getUserId());
         assertThat(actual).isEqualTo(expected);
@@ -37,8 +43,29 @@ public class UserDaoTest {
 
     @Test
     public void findAll() {
-        UserDao userDao = new UserDao();
         List<User> users = userDao.findAll();
         assertThat(users).hasSize(1);
     }
+
+    @DisplayName("로그인을 테스트 합니다.")
+    @Test
+    void loginTest() {
+        User user = new User("userId1", "password", "name", "javajigi@email.com");
+        userDao.insert(user);
+
+        User result = userDao.findByEmailAndPassword(user.getEmail(), user.getPassword());
+        assertThat(result).isEqualTo(user);
+    }
+
+    @DisplayName("id가 두개일때 에러를 발생 시킵니다.")
+    @Test
+    void manyQueryException() {
+        userDao.insert(new User("userId1", "password", "name", "javajigi@email.com"));
+        userDao.insert(new User("userId2", "password", "name", "javajigi@email.com"));
+
+        assertThatExceptionOfType(ManyResultQueryException.class).isThrownBy(() -> {
+            userDao.findByEmailAndPassword("javajigi@email.com", "password");
+        });
+    }
+
 }
