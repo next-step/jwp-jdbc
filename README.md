ef20ba4536d0e8f2c6bcc82ed3080190166a15c5
ohminho <ohminho@woowahan.com>
2022-09-09 17:33:05 +0900

    feature - JsonView 구현

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 
 ## 온라인 코드 리뷰 과정
 * [텍스트와 이미지로 살펴보는 온라인 코드 리뷰 과정](https://github.com/next-step/nextstep-docs/tree/master/codereview)
+
+# 1단계 - REST API 및 테스트 리팩토링
+
+* JsonViewTest 통과 하도록 JsonView 구현
+* UserAcceptanceTest 통과 하도록 UserAcceptanceController 추가  
--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,47 @@
 package core.mvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 
+        if (model.isEmpty()) {
+            return;
+        }
+
+        if (model.size() == 1) {
+            writeModel(getFirstValue(model), response);
+            return;
+        }
+
+        writeModel(model, response);
+    }
+
+    private Object getFirstValue(Map<String, ?> model) {
+        return model.values()
+                .stream()
+                .findFirst()
+                .stream()
+                .findFirst()
+                .orElseThrow();
+    }
+
+    private void writeModel(Object json, HttpServletResponse response) throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.print(MAPPER.writeValueAsString(json));
+        writer.flush();
     }
 }
