a34d167e44f68533307da7b714c8afaab8a4171c
kyun <annakky@naver.com>
2022-09-11 23:20:45 +0900

    feat: json view 기능 추가

--- src/main/java/core/mvc/JsonUtils.java
+++ src/main/java/core/mvc/JsonUtils.java
@@ -1,6 +1,7 @@
 package core.mvc;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -16,6 +17,15 @@ public class JsonUtils {
             return objectMapper.readValue(json, clazz);
         } catch (IOException e) {
             throw new ObjectMapperException(e);
+        }
+    }
+
+    public static String objectToJson(Object object) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException("OBJECT TO JSON PARSE ERROR");
         }
     }
 }
