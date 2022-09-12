c8aaac83e29c683742cc363dbe5f2a154eee40ef
hun2 <hun2@woowahan.com>
2022-09-12 14:51:54 +0900

    step1-2 리팩토링

--- src/main/java/next/controller/UserRestController.java
+++ src/main/java/next/controller/UserRestController.java
@@ -14,6 +14,7 @@ import next.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,50 +22,44 @@ import java.io.IOException;
 import java.util.stream.Collectors;
 
 @Controller
-public class UserApiController {
-    private static final Logger logger = LoggerFactory.getLogger(UserApiController.class);
+public class UserRestController {
+    private static final Logger logger = LoggerFactory.getLogger(UserRestController.class);
+    private static final String BASE_URI = "/api/users";
 
-    @RequestMapping(value = "/api/users", method = RequestMethod.POST)
-    public ModelAndView create(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String content = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-        UserCreatedDto userCreatedDto = JsonUtils.toObject(content, UserCreatedDto.class);
-
+    @RequestMapping(value = BASE_URI, method = RequestMethod.POST)
+    public ModelAndView create(@RequestBody UserCreatedDto userCreatedDto, HttpServletResponse response) throws IOException {
         User user = new User(userCreatedDto.getUserId(), userCreatedDto.getPassword(), userCreatedDto.getName(), userCreatedDto.getEmail());
         logger.debug("User: {}", userCreatedDto);
         DataBase.addUser(user);
 
-        ModelAndView modelAndView = new ModelAndView(new JsonView());
-        modelAndView.addObject("user", user);
-
         response.setStatus(HttpStatus.CREATED.value());
         response.setHeader("Location", "/api/users/" + userCreatedDto.getUserId());
+
+        ModelAndView modelAndView = new ModelAndView(new JsonView());
+        modelAndView.addObject("user", user);
         return modelAndView;
     }
 
-    @RequestMapping(value = "/api/users/{userId}", method = RequestMethod.PUT)
-    public ModelAndView update(@PathVariable String userId, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        ModelAndView modelAndView = new ModelAndView(new JsonView());
-        String content = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-        UserUpdatedDto userUpdatedDto = JsonUtils.toObject(content, UserUpdatedDto.class);
-
+    @RequestMapping(value = BASE_URI + "/{userId}", method = RequestMethod.PUT)
+    public ModelAndView update(@PathVariable String userId, @RequestBody UserUpdatedDto userUpdatedDto, HttpServletResponse response) throws IOException {
         logger.debug("userId : {}", userId);
 
         User user = DataBase.findUserById(userId);
         user.update(new User(userId, user.getPassword(), userUpdatedDto.getName(), userUpdatedDto.getEmail()));
 
         response.setStatus(HttpStatus.OK.value());
-        return modelAndView;
+        return new ModelAndView(new JsonView());
     }
 
-    @RequestMapping(value = "/api/users/{userId}", method = RequestMethod.GET)
+    @RequestMapping(value = BASE_URI +"/{userId}", method = RequestMethod.GET)
     public ModelAndView findUser(@PathVariable String userId, HttpServletResponse response) {
-        ModelAndView modelAndView = new ModelAndView(new JsonView());
         logger.debug("userId : {}", userId);
 
         User user = DataBase.findUserById(userId);
-        modelAndView.addObject("user", user);
-
         response.setStatus(HttpStatus.OK.value());
+
+        ModelAndView modelAndView = new ModelAndView(new JsonView());
+        modelAndView.addObject("user", user);
         return modelAndView;
     }
 }
