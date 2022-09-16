ce08f33fd594e117350c1763301f098bb558871d
JT <38105420+wjdxor@users.noreply.github.com>
2022-09-16 13:49:50 +0900

    Step1 REST API 및 테스트 리팩토링 (#194)
    * docs: 1단계 요구사항 문서작성
    
    * feat: JsonView구현
    
    * feat: RequestBodyArgumentResolver 구현
    
    * feat: ControllerScanner에 등록
    
    * feat: RequestBody 애노테이션 생성
    
    * feat: REST API Controller 구현

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,34 @@
 package core.mvc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class JsonView implements View {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        if (!model.isEmpty()) {
+            String content = extractContent(model);
+            PrintWriter writer = response.getWriter();
+            writer.write(content);
+            writer.close();
+        }
+    }
 
+    private String extractContent(Map<String, ?> model) throws JsonProcessingException {
+        List<Entry<String, ?>> entries = new ArrayList<>(model.entrySet());
+        Object value = entries.get(0).getValue();
+        return objectMapper.writeValueAsString(value);
     }
 }
