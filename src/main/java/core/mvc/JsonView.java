48d3890bce5aaa6fd299105a239e5f072f0d429c
joel-you <ruew12@gmail.com>
2022-10-10 17:12:47 +0900

    feature: RequestBodyArgumentResolver 구현
    * ReqeustBody 어노테이션 생성
    * ReqeustBodyArgumentResolver 구현
    * JsonView 에서 Json으로 처리할 수 있도록 수정
    * UserRestController 구현하여 변경 사항을 적용함.
    * ApiUser.http 를 생성하여 테스트 진행

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,46 @@
 package core.mvc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.util.CollectionUtils;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class JsonView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String content = extractContent(model);
 
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        PrintWriter writer = response.getWriter();
+        writer.write(content);
+        writer.flush();
+        writer.close();
+    }
+
+    private String extractContent(Map<String, ?> model) {
+        if (CollectionUtils.isEmpty(model)) {
+            return "";
+        }
+
+        if (model.size() == 1) {
+            return objectToString(model.values().stream()
+                    .findFirst()
+                    .orElseThrow(IllegalArgumentException::new));
+        }
+
+        return objectToString(model);
+    }
+
+    private String objectToString(Object model) {
+        try {
+            return new ObjectMapper().writeValueAsString(model);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(String.format("Cannot convert %s to Json type.", model));
+        }
     }
 }
