6c4fcb96931f84f7fedd5d20fdad40c58780a217
김용권 <dydrnjs518@cnspartner.com>
2022-10-28 16:37:07 +0900

    feat: jsonView 구현

--- src/main/java/core/mvc/JsonView.java
+++ src/main/java/core/mvc/JsonView.java
@@ -1,12 +1,51 @@
 package core.mvc;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Optional;
 
 public class JsonView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String result = convertObjectToJson(model);
 
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        if(result != null) {
+            response.getWriter().write(result);
+        }
+    }
+
+    private static String convertObjectToJson(Map<String, ?> model) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        String result = null;
+        if(isSingleModel(model)) {
+            Iterator<String> keys = model.keySet().iterator();
+            result = mapper.writeValueAsString(model.get(keys.next()));
+        } else if(isMultipleModels(model)){
+            result = mapper.writeValueAsString(model);
+        }
+
+        return result;
+    }
+
+    private static boolean isMultipleModels(Map<String, ?> model) {
+        return model.size() > 1;
+    }
+
+    private static boolean isSingleModel(Map<String, ?> model) {
+        return model.size() == 1;
     }
 }
